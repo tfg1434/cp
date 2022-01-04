c1b5294bdea87bc24feac6bb7eabf3177071e1ad
torontofangirl <toronto.tristan@gmail.com>
2022-01-04 15:18:52 -0600

    use cpbooster instead of autocp

--- Codeforces/CodeforcesCodeforcesRound763Div.2/A_RobotCleaner.cpp
+++ Codeforces/CodeforcesCodeforcesRound763Div.2/A_RobotCleaner.cpp
@@ -1,4 +1,4 @@
-﻿//  A. Robot Cleaner
+//  A. Robot Cleaner
 
 #include<bits/stdc++.h>
 
@@ -19,23 +19,23 @@ int main() {
     for (int c = 0; c < C; c++) {
         int n, m, rb, cb, rd, cd;
         scanf("%d %d %d %d %d %d", &n, &m, &rb, &cb, &rd, &cd);
-        
+
         int dr = 1;
         int dc = 1;
         int t = 0;
-        
+
         if (didClean(rb, cb, rd, cd)) {
             cout << t << '\n';
             continue;
         }
-        
+
         while (rb != rd && cb != cd){
             if (rb + dr > n)
                 dr = -dr;
-            
+
             if (cb + dc > m)
                 dc = -dc;
-            
+
             rb += dr;
             cb += dc;
             t++;
@@ -43,8 +43,8 @@ int main() {
 
         cout << t << '\n';
     }
-    
-    
+
+
     
 
     return 0;
