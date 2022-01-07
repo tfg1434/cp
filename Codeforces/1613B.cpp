d68613015526ef7b24cdb2a5b1bbe9b9cffe150a
torontofangirl <toronto.tristan@gmail.com>
2022-01-07 05:30:54 -0600

    1613B

--- Codeforces/1613B.cpp
+++ Codeforces/1613B.cpp
@@ -12,39 +12,28 @@ int main() {
     for (int c = 0; c < C; ++c) {
         int n;
         cin >> n;
-        //https://codeforces.com/problemset/problem/1613/B
-        
-        unordered_set<int> sett;
-        vector<int> a(n);
+
+        vector <int> a(n);
         for (int i = 0; i < n; ++i) {
-            int in;
-            cin >> in;
-            
-            sett.insert(in);
-            a[i] = in;
-        }
-
-        int count = floor(n / 2);
-        int found = 0;
-
-        for (int i = 0; i < a.size(); ++i) {
-            for (int j = 0; j < a.size(); ++j) {
-                int x = a[i];
-                int y = a[j];
-                
-                if (i == j) continue;
-                if (x == y) continue;
-                if (sett.count(x % y)) continue;
-                
-                found++;
-                cout << x << " " << y << '\n';
-                
-                if (found == count)
-                    goto done;
-            }
+            cin >> a[i];
         }
         
-        done: ;
+        auto elem = std::min_element(a.begin(), a.end());
+        int index = elem - a.begin();
+        int y = *elem;
+        int target = n/2;
+        int count = 0;
+
+        for (int i = 0; i < n; ++i) {
+            int x = a[i];
+            
+            if (x == y) continue;
+            
+            ++count;
+            cout << x << " " << y << '\n';
+            
+            if (count == target) break;
+        }
     }
 
     return 0;
