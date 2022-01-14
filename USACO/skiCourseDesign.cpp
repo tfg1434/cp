89e0f8c96da1e057dedd79e3255cdbbbdab81fb5
torontofangirl <toronto.tristan@gmail.com>
2022-01-13 19:30:01 -0600

    skiin'

--- USACO/skiCourseDesign.cpp
+++ USACO/skiCourseDesign.cpp
@@ -1,11 +1,15 @@
-﻿#include <bits/stdc++.h>
+﻿/*
+ID: toronto4
+TASK: skidesign
+LANG: C++14
+*/
+#include <bits/stdc++.h>
 using namespace std;
 
 #define ll long long
 
-void reduceOne(vector<int>& h) {
-    
-}
+constexpr int max_n = 1000;
+constexpr int max_m = 100;
 
 int main() {
     freopen("skidesign.in", "r", stdin);
@@ -18,13 +22,26 @@ int main() {
     vector<int> h(N);
     for (int i = 0; i < N; i++)
         cin >> h[i];
-    vector<int> costs(N, 0);
-    
-    int dy = (h[N-1] - h[0] - 17) / 2;
-    
-    //auto minMax = minmax_element(h.begin(), h.end());
 
+    int minCost = 99999999;
+    //consider cost of all ranges with max dy =17
+    for (int i = 0; i <= 83; ++i) {
+        int up = i + 17;
+        int cost = 0;
+        int x;
+
+        for (int j = 0; j < N; ++j) {
+            if (h[j] > up) x = h[j] - up;
+            else if (h[j] < i) x = i - h[j];
+            else x = 0;
+            
+            cost += x*x;
+        }
+        
+        minCost = min(minCost, cost);
+    }
     
+    cout << minCost << endl;
     
     return 0;
 }
