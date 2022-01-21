5025cdd0ef92332f9266851ecd47c4fbbbb1885e
torontofangirl <toronto.tristan@gmail.com>
2022-01-21 08:40:12 -0600

    AAAAAAAAAAAA

--- USACO/healthyHolsteins.cpp
+++ USACO/healthyHolsteins.cpp
@@ -14,24 +14,40 @@ vector<vector<int>> feed;
 vector<vector<int>> ans { };
 vector<int> curr { };
 
-void solve() {
-    bool ok = true;
+bool isOk(const vector<int>& v) {
     vector<int> vitamins(V, 0);
-    for (int scoopIdx = 0; scoopIdx < curr.size(); scoopIdx++) {
+    for (int scoopIdx : v) {
         for (int vitIdx = 0; vitIdx < V; ++vitIdx) {
-            vitamins[vitIdx] += feed[curr[scoopIdx]][vitIdx];
+            vitamins[vitIdx] += feed[scoopIdx][vitIdx];
         }
     }
     for (int i = 0; i < vitamins.size(); ++i) {
         if (vitamins[i] < required[i]) {
-            ok = false;
-            break;
+            return false;
         }
     }
-    if (ok) {
-        ans.push_back(curr);
-        return;
-    }
+    
+    return true;
+}
+
+void solve() {
+//    bool ok = true;
+//    vector<int> vitamins(V, 0);
+//    for (int scoopIdx = 0; scoopIdx < curr.size(); scoopIdx++) {
+//        for (int vitIdx = 0; vitIdx < V; ++vitIdx) {
+//            vitamins[vitIdx] += feed[curr[scoopIdx]][vitIdx];
+//        }
+//    }
+//    for (int i = 0; i < vitamins.size(); ++i) {
+//        if (vitamins[i] < required[i]) {
+//            ok = false;
+//            break;
+//        }
+//    }
+//    if (ok) {
+    ans.push_back(curr);
+//        return;
+//    }
     
     for (int i = 0; i < feed.size(); ++i) {
         if (std::count(curr.begin(), curr.end(), i) > 0) continue;
@@ -64,26 +80,48 @@ int main() {
         
     solve();
 
-    sort(ans.begin(), ans.end(), [](const vector<int>& a, const vector<int>& b) {
-        return a.size() < b.size();
-    });
-    unsigned ll siz = ans[0].size();
-    vector<int> sol;
-    int minCost = INT_MAX;
+//    sort(ans.begin(), ans.end(), [](const vector<int>& a, const vector<int>& b) {
+//        return a.size() < b.size();
+//    });
+//    unsigned ll siz = ans[0].size();
+//    vector<int> sol;
+//    int minCost = INT_MAX;
+//    for (const vector<int>& a:ans) {
+//        if (a.size() != siz) continue;
+//        int cost = 0;
+//        for (int c:a) cost += c;
+//        
+//        if (cost < minCost) {
+//            sol = a;
+//            minCost = cost;
+//        }
+//    }
+    vector<int>::size_type minSize = INT_MAX;
+    vector<vector<int>> sol;
     for (const vector<int>& a:ans) {
-        if (a.size() != siz) continue;
+        if (!isOk(a)) continue;
+        
+        sol.push_back(a);
+        minSize = min(minSize, a.size());
+    }
+    
+    cout << minSize;
+    
+    int minCost = INT_MAX;
+    vector<int> a;
+    for (auto s:sol) {
+        if (s.size() != minSize) continue;
+        
         int cost = 0;
-        for (int c:a) cost += c;
+        for (int c:s) cost += c;
         
         if (cost < minCost) {
-            sol = a;
+            a = s;
             minCost = cost;
         }
     }
-
-    sort(sol.begin(), sol.end());
-    cout << sol.size();
-    for (int i : sol) {
+    
+    for (int i : a) {
         cout << " " << i+1;
     }
     cout << endl;
