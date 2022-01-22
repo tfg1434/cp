368a0429099b9a9ecb53026092d65dd41166af09
torontofangirl <toronto.tristan@gmail.com>
2022-01-22 06:51:14 -0600

    hammingCodes.cpp

--- USACO/hammingCodes.cpp
+++ USACO/hammingCodes.cpp
@@ -1,75 +1,72 @@
-﻿#include <bits/stdc++.h>
+﻿/*
+ID: toronto4
+TASK: hamming
+LANG: C++14
+*/
+#include <bits/stdc++.h>
 using namespace std;
 
 #define ll long long
 
-int N, B, D;
-vector<vector<bool>> codewords;
+constexpr int n_max = 65;
+constexpr int b_max = 10;
+constexpr int d_max = 10;
+constexpr int max_u8 = (1 << 8) + 1;
+int N, B, D, searchTo;
+int codewords[n_max];
+int dist[max_u8][max_u8];
 
-vector<bool> toBin(int n){
-    vector<bool> res;
-    while(n!=0) {
-        res.insert(res.begin(), n % 2 != 0);
-        n/=2;
+void solve(int curr, int start){
+    if (curr == N) {
+        for (int i = 0; i < N; i++) {
+            if (i%10 != 0) cout << ' ';
+            cout << codewords[i];
+            if (i%10==9 || i == curr-1) cout << '\n';
+        }
+
+        exit(0);
     }
-    while (res.size() <= B) res.insert(res.begin(), false);
     
-    return res;
-}
+    bool ok;
+    
+    for (int i = start; i < searchTo; ++i) {
+        ok = true;
 
-int toDec(vector<bool> bin){
-    int res = 0;
-    for (int i = 0; i < bin.size(); i++) {
-        if (bin[i]) res += pow(2, i);
-    }
-    return res;
-}
-
-int hammingDist(vector<bool> a, vector<bool> b){
-    int res = 0;
-    for(int i=0; i<a.size(); i++){
-        if(a[i] != b[i]) res++;
-    }
-    return res;
-}
-
-int main() {
-//    freopen("hamming.in", "r", stdin);
-//    freopen("hamming.out", "w", stdout);
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    cin >> N >> B >> D;
-    vector<bool> zero(B, false);
-    codewords.reserve(N);
-    codewords.push_back(zero);
-
-    for (int n = 1; n < /*pow(2, B)*/INT_MAX; ++n) {
-        if (codewords.size() == N) break;
-        
-        vector<bool> asBin = toBin(n);
-        bool ok = true;
-        
-        for (int j = 0; j < codewords.size(); j++) {
-            vector<bool> codeword = codewords[j];
-            
-            if (hammingDist(codeword, asBin) < D) {
+        for (int j = 0; j < curr; ++j) {
+            if (dist[codewords[j]][i] < D) {
                 ok = false;
                 break;
             }
         }
         
-        if (ok) codewords.push_back(asBin);
+        if (ok) {
+            codewords[curr] = i;
+            solve(curr+1, i+1);
+        }
     }
-    sort(codewords.begin(), codewords.end(), [](vector<bool> a, vector<bool> b){
-        return toDec(a) < toDec(b);
-    });
+}
 
-    for (int i = 0; i < N; i++) {
-        if (i % 10 == 0 && i > 0)
-            cout << '\n';
-        cout << ((i % 10 == 0) ? to_string(toDec(codewords[i])) : " " + to_string(toDec(codewords[i])));
+int main() {
+    freopen("hamming.in", "r", stdin);
+    freopen("hamming.out", "w", stdout);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N >> B >> D;
+    searchTo = (1 << B);
+    codewords[0] = 0;
+
+    for (int i = 0; i < max_u8; ++i) {
+        for (int j = 0; j < max_u8; ++j) {
+            dist[i][j] = 0;
+
+            for (int k = 0; k < B; ++k) {
+                dist[i][j] += ((1 << k) & i) != ((1 << k) & j);
+            }
+        }
     }
+    
+    solve(1, 1);
 
     return 0;
 }
