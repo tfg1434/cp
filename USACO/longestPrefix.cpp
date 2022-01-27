5beea76c4ae53103a687bc35d2901d1c37e1b0d4
torontofangirl <toronto.tristan@gmail.com>
2022-01-27 06:46:32 -0600

    ???

--- USACO/longestPrefix.cpp
+++ USACO/longestPrefix.cpp
@@ -12,6 +12,14 @@ constexpr int MAX_P_LEN = 10;
 vector<string> prims;
 string S;
 set<string> prefixes { "" };
+set<string> tried;
+
+bool startsWith(string mainStr, string toMatch) {
+    for (int i = 0; i < toMatch.size(); ++i) 
+        if (mainStr[i] != toMatch[i]) return false;
+    
+    return true;
+}
 
 int main() {
     ios::sync_with_stdio(0);
@@ -30,23 +38,28 @@ int main() {
         bool found = false;
         
         for (string prefix:prefixes) {
+//            if (tried.count(prefix)) continue;
+//            tried.insert(prefix);
+            
             if (S.rfind(prefix, 0) == 0) {
                 for (string p:prims) {
-                    if (S.rfind(prefix + p, 0) == 0) {
-                        prefixes.insert(prefix + p);
+                    string look = prefix + p;
+                    cout << startsWith(S, look) << endl; 
+                    if (S.rfind(look, 0) == 0) {
+                        prefixes.insert(look);
                         found = true;
                     }
-                } 
+                }
             } 
         } 
         
         if (!found) break;
     }
 
-    const auto longest = std::max_element(prefixes.cbegin(), prefixes.cend(),
-                                  [](const std::string& lhs, const std::string& rhs) { return lhs.size() < rhs.size(); });
+    const auto longest = max_element(prefixes.cbegin(), prefixes.cend(),
+                                  [](const string& lhs, const string& rhs) { return lhs.size() < rhs.size(); });
     
-    cout << *longest->size() << '\n';
+    cout << longest->size() << '\n';
     
     return 0;
 }
