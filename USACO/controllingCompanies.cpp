c86a4ee63d6071e8a65c0b6d615335678f9c90f1
torontofangirl <toronto.tristan@gmail.com>
2022-01-29 15:17:01 -0600

    controllingCompanies.cpp

---
+++ USACO/controllingCompanies.cpp
@@ -0,0 +1,78 @@
+﻿/*
+ID: toronto4
+TASK: concom 
+LANG: C++14
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+constexpr int max_value = 101;
+int N;
+int arr[max_value][max_value];
+bool controlling[max_value][max_value];
+
+void updateControl(int I, int J) {
+    if (controlling[I][J]) return;
+    controlling[I][J] = true;
+    
+    //I 's holdings to J
+    for (int i = 0; i < max_value; i++) 
+        arr[I][i] += arr[J][i];
+    
+    //controlling I == controlling J
+    for (int i = 0; i < max_value; ++i) {
+        if (controlling[i][I])
+            updateControl(i, J);
+    }
+    
+    //updateControl I's controls
+    for (int i = 0; i < max_value; ++i) {
+        if (arr[I][i] > 50)
+            updateControl(I, i);
+    }
+}
+
+void updatePercent(int I, int J, int p) {
+    //update %
+    for (int i = 0; i < max_value; ++i) {
+        if (controlling[i][I])
+            arr[i][J] += p;
+    }
+
+    //look for new owners
+    for (int i = 0; i < max_value; ++i) {
+        if (arr[i][J] > 50)
+            updateControl(i, J);
+    }
+}
+
+int main() {
+    freopen("concom.in", "r", stdin);
+    freopen("concom.out", "w", stdout);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    //own yourself
+    for (int i = 0; i < max_value; ++i) controlling[i][i] = true; 
+        
+    cin >> N;
+    for (int n = 0; n < N; ++n) {
+        int I, J, P;
+        cin >> I >> J >> P;
+        
+        updatePercent(I, J, P); 
+    }
+
+    for (int i = 0; i < max_value; ++i) {
+        for (int j = 0; j < max_value; ++j) {
+            if (i == j) continue;
+            
+            if (controlling[i][j])
+                cout << i << " " << j << '\n';
+        }
+    }
+
+    return 0;
+}
---
+++ USACO/moneySystems.cpp
@@ -0,0 +1,34 @@
+﻿/*
+ID: toronto4
+TASK: money 
+LANG: C++14
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+constexpr int MAX_N = 10'000;
+constexpr int MAX_V = 25;
+int N, V;
+ll dp[MAX_N+1];
+int coins[MAX_V];
+
+int main() {
+    freopen("money.in", "r", stdin);
+    freopen("money.out", "w", stdout);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> V >> N;
+    for (int i = 0; i < V; ++i) cin >> coins[i];
+    dp[0] = 1;
+
+    for (int i = 0; i < V; ++i) 
+        for (int j = 0; j <= N; ++j) 
+            if (j + coins[i] <= N) dp[j + coins[i]] += dp[j];
+        
+    cout << dp[N] << '\n';
+
+    return 0;
+}
