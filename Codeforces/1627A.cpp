76db757059956d5cbf4b9509f86a0fa00e67c851
torontofangirl <toronto.tristan@gmail.com>
2022-01-31 06:45:47 -0600

    started 1627A

---
+++ Codeforces/1627A.cpp
@@ -0,0 +1,18 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int T, R, C, N, M;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> T;
+    while (T--) {
+        
+    }
+
+    return 0;
+}
