ee15ee25d1ac5bf994e7b12dac736a36811ee084
torontofangirl <toronto.tristan@gmail.com>
2022-02-04 14:48:01 -0600

    1617B.cpp

---
+++ Codeforces/1617B.cpp
@@ -0,0 +1,32 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int T, N;
+
+int gcd(int a, int b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> T;
+    while (T--) {
+        cin >> N;
+        int b;
+
+        for (int a = 2; true; ++a) {
+            b = N - 1 - a;
+            
+            if (gcd(a, b) == 1) {
+                cout << a << ' ' << b << ' ' << 1 << endl;
+                break;
+            }
+        } 
+    }
+
+    return 0;
+}
