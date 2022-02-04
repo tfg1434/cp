1d197ebdcdc1003c6ec8502fde35dd6c8499e054
torontofangirl <toronto.tristan@gmail.com>
2022-02-04 13:07:48 -0600

    nope

---
+++ Codeforces/1617C.cpp
@@ -0,0 +1,44 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int T, N;
+
+void solve() {
+    cin >> N;
+    vector<int> a(N);
+    for (int i = 0; i < N; i++) {
+        cin >> a[i];
+    }
+    std::sort(a.begin(), a.end());
+    int ans = 0;
+
+    for (int i = 0; i < a.size(); ++i) {
+        int item = a[i];
+        int n = i+1;
+
+        if (item != n) {
+            if (ceil(item/2.0) <= n) {
+                cout << -1 << endl;
+                return;
+            }
+            
+            ans++;
+        }
+    }
+    
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> T;
+    while (T--) {
+        solve(); 
+    }
+
+    return 0;
+}
