9691ca7c97dbcea3ce7a9345f062ab75fe1617f0
torontofangirl <toronto.tristan@gmail.com>
2022-02-05 14:33:50 -0600

    1611C.cpp

--- Codeforces/1611C.cpp
+++ Codeforces/1611C.cpp
@@ -16,20 +16,15 @@ int main() {
         vector<int> a(n);
         for (int& item : a) cin >> item;
         
-        int me = *max_element(a.begin(), a.end());
-        if ( !(a[0] == me || a[a.size() - 1] == me) ) {
+        if ( !(a[0] == n || a[a.size() - 1] == n) ) {
             cout << -1 << endl;
             continue;
         }
         
-        vector<int> ans;
-        for (int l = 0; l < n / 2; ++l) {
-            int r = n - l - 1;
-            ans.insert(ans.begin(), a[l]); ans.push_back(a[r]);
-        }
-        if (n % 2 != 0) ans.push_back(a[n / 2]);
-
-        for (const int item : ans) cout << item << " ";
+        std::reverse(a.begin(), a.end());
+        //allowed to just reverse because you will only take from one side
+        //until last element
+        for (const int item : a) cout << item << " ";
         cout << endl;
     }
 
