f1758ed0730f9f0e420c64f202367b1037aff7c7
torontofangirl <toronto.tristan@gmail.com>
2022-02-05 05:49:20 -0600

    too slow

---
+++ Codeforces/1613C.cpp
@@ -0,0 +1,39 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int T, N, H;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> T;
+    while (T--) {
+        cin >> N >> H;
+        vector<int> a(N);
+        for (int i = 0; i < N; i++) cin >> a[i];
+        
+        int k = 0;
+        vector<int> dt(N - 1);
+        for (int i = 0; i < N - 1; i++) {
+            dt[i] = a[i + 1] - a[i];
+        }
+        while (true) {
+            int total = 0;
+            for (int i = 0; i < N - 1; ++i) {
+                total += min(k, dt[i]); 
+            }
+            total += k; //last hit
+            
+            if (total >= H) break;
+            
+            k++;
+        }
+        
+        cout << k << '\n';
+    }
+
+    return 0;
+}
