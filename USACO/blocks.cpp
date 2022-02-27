369d9557d03e89555073f0d41cc32d4628e602e4
torontofangirl <toronto.tristan@gmail.com>
2022-02-27 09:23:55 -0600

    blocks.cpp

--- USACO/blocks.cpp
+++ USACO/blocks.cpp
@@ -5,22 +5,20 @@ using namespace std;
 #define rall(x) (x).rbegin(), (x).rend()
 
 int T;
-vector<set<char>> blocks(4);
+set<char> blocks[4];
+vector<vector<int>> perms;
 
 
-void solve(string word, set<int> used, int depth) {
-    if (depth == 3) {
-        cout << "YES" << '\n';
+void perm(vector<int> a, int n, int k, int i) {
+    if (i == 0) {
+        perms.push_back(a);
+        return;
     }
 
-    for (int i = 0; i < blocks.size(); i++) {
-        if (used.count(i)) continue;
-
-        if (blocks[i].count(word[depth])) {
-            used.insert(i);
-            solve(word, used, depth + 1);
-            used.erase(i);
-        }
+    for (int ii = 0; ii < n; ii++) {
+        swap(a[ii], a[n-1]);
+        perm(a, n-1, k, i-1);
+        swap(a[ii], a[n-1]);
     }
 }
 
@@ -32,12 +30,38 @@ int main() {
     for (int i = 0; i < 4; i++) {
         blocks[i].clear();
         string s; cin >> s;
+//        cout << s << endl;
         for (char c : s) blocks[i].insert(c);
     }
 
+    vector<int> a = {0, 1, 2, 3};
     for (int i = 0; i < n; i++) {
         string word; cin >> word;
-        solve(word, set<int>(), 0);
+//        cout << word << endl;
+        perms.clear();
+        perm(a, a.size(), word.size(), word.size());
+        
+        bool ok = false;
+        for (auto &p : perms) {
+//            cout << p[0] << p[1] << p[2] << p[3] << endl;
+            bool ok2 = true;
+            for (int j = 0; j < word.size(); j++) {
+                if (blocks[p[j]].count(word[j]) == 0) {
+                    ok2 = false;
+                    break;
+                }
+            }
+            if (ok2) {
+                ok = true;
+                break;
+            }
+        }
+
+        if (word == "CODE") {
+            int breakpoint = 0;
+        }
+        if (ok) cout << "YES" << endl;
+        else cout << "NO" << endl;
     }
     
     return 0;
