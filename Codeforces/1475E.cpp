68acd4232a168236a91196ea50bc756b81fad161
torontofangirl <toronto.tristan@gmail.com>
2022-03-27 04:50:24 -0500

    1475E

---
+++ Codeforces/1475E.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pii = pair<int, int>;
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define IN(A, B, C) assert(B <= A && A <= C)
+
+constexpr int MOD = 1e9 + 7;
+constexpr int INF = 1e9;
+constexpr ll INFF = 1e18;
+
+
+int factmod(int n, int p) {
+    vector<int> f(p);
+    f[0] = 1;
+    for (int i = 1; i < p; i++)
+        f[i] = f[i-1] * i % p;
+
+    int res = 1;
+    while (n > 1) {
+        if ((n/p) % 2)
+            res = p - res;
+        res = res * f[n%p] % p;
+        n /= p;
+    }
+    return res;
+}
+
+int nCrModp(int n, int r, int p)
+{
+    // Optimization for the cases when r is large
+    if (r > n - r)
+        r = n - r;
+ 
+    // The array C is going to store last row of
+    // pascal triangle at the end. And last entry
+    // of last row is nCr
+    int C[r + 1];
+    memset(C, 0, sizeof(C));
+ 
+    C[0] = 1; // Top row of Pascal Triangle
+ 
+    // One by constructs remaining rows of Pascal
+    // Triangle from top to bottom
+    for (int i = 1; i <= n; i++) {
+ 
+        // Fill entries of current row using previous
+        // row values
+        for (int j = min(i, r); j > 0; j--)
+ 
+            // nCj = (n-1)Cj + (n-1)C(j-1);
+            C[j] = (C[j] + C[j - 1]) % p;
+    }
+    return C[r];
+}
+
+void solve() {
+    int n, k; cin >> n >> k;
+    vector<int> cnt(n+1);
+    for (int i = 0; i < n; i++) {
+        int x; cin >> x;
+        cnt[x]++;
+    }
+
+    for (int i = n; i >= 0; i--) {
+        if (cnt[i] >= k) {
+            cout << nCrModp(cnt[i], k, MOD) << endl;
+            return;
+
+        } else {
+            k -= cnt[i];
+        }
+    }
+    // cout << 1;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int T; cin >> T; while (T--) {
+        solve();
+    }    
+    
+    return 0;
+}
+
