239cab5b0ba2ea9a18e871c73c38178fb0f8c407
torontofangirl <toronto.tristan@gmail.com>
2022-03-28 05:00:29 -0500

    651C

--- Codeforces/651C.cpp
+++ Codeforces/651C.cpp
@@ -25,29 +25,61 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     
+    map<int, ll> cx, cy;
+    map<pii, ll> mp;
     int n; cin >> n;
-    map<int, int> cx, cy;
-    set<pii> st;
     for (int i = 0; i < n; i++) {
         int x, y; cin >> x >> y;
-        cx[x]++; cy[y]++;
-        st.insert({x, y});
+        cx[x]++;
+        cy[y]++;
+        mp[{x, y}]++;
     }
 
-    int ans = 0;
-    set<int> usedX;
-    for (auto [k, v] : cx) {
-        if (v == 1) continue;
-        ans += nCr(v, 2);
-        usedX.insert(k);
+    ll ans = 0;
+    auto iter = cx.begin();
+    while (iter != cx.end()) {
+        //choose
+        ans += (iter->second * (iter->second - 1)) / 2;
+        iter++;
     }
-    for (auto [k, v] : cy) {
-        if (v == 1) continue;
-        ans += nCr(v, 2);
-        if (usedX.count(k)) ans--;
+    iter = cy.begin();
+    while (iter != cy.end()) {
+        ans += (iter->second * (iter->second - 1)) / 2;
+        iter++;
+    }
+
+    //mp is frequency array of (x, y)
+    auto iter2 = mp.begin();
+    while (iter2 != mp.end()) {
+        ans -= (iter2->second * (iter2->second - 1)) / 2;
+        iter2++;
     }
 
     cout << ans << endl;
+
+
+    // map<int, int> cx, cy;
+    // set<pii> st;
+    // for (int i = 0; i < n; i++) {
+        // int x, y; cin >> x >> y;
+        // cx[x]++; cy[y]++;
+        // st.insert({x, y});
+    // }
+
+    // int ans = 0;
+    // set<pii> used;
+    // for (auto [x, y] : st) {
+        // if (cx[x] == 1) continue;
+        // ans += nCr(cx[x], 2);
+        // used.insert({x, y});
+    // }
+    // for (auto [x, y] : st) {
+        // if (cy[y] == 1) continue;
+        // ans += nCr(cy[y], 2);
+        // if (used.count({ x, y })) ans--;
+    // }
+
+    // cout << ans << endl;
     
     return 0;
 }
