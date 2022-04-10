b51581f5f5e4289a7656c3ad488143356976d942
torontofangirl <toronto.tristan@gmail.com>
2022-04-10 17:04:59 -0500

    1455D

---
+++ Codeforces/1455D.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<long long, long long>;
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define IN(A, B, C) assert(B <= A && A <= C)
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+constexpr int MOD = 1e9 + 7;
+constexpr int INF = 1e9;
+constexpr ll INFF = 1e18;
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int T; cin >> T; while (T--) {
+        ll n, X; cin >> n >> X;
+        vector<ll> a(n);
+        for (auto &x : a) cin >> x;
+
+        auto leftest = [&](ll x) {
+            for (ll i = 0; i < n; ++i) {
+                if (a[i] > x) return i;
+            }
+            return INFF;
+        };
+
+        bool ok = true;
+        ll ans = 0;
+        while (!is_sorted(all(a))) {
+            ll l = leftest(X);
+            if (l == INFF) {
+                ok = false;
+                break;
+            }
+
+            swap(X, a[l]);
+            ans++;
+        }
+
+        if (ok) cout << ans << '\n';
+        else cout << -1 << '\n';
+    }    
+    
+    return 0;
+}
+
