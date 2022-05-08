1e862680f9cb976cdf1be24519679c2c12900dd1
torontofangirl <toronto.tristan@gmail.com>
2022-05-08 07:07:29 -0500

    1542C

---
+++ Codeforces/1542C.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<long long, long long>;
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define IN(A, B, C) assert(B <= A && A <= C)
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+constexpr int MOD = 1e9 + 7;
+constexpr int INF = 1e9;
+constexpr ll INFF = 1e18;
+
+
+inline int gcd(int a, int b) { return (b == 0) ? a : gcd(b, a % b); }
+
+inline int lcm(int a, int b) { return a / gcd(a, b) * b; }
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int T; cin >> T; while (T--) {
+        ll n; cin >> n;
+        ll g = 1, ans = 0;
+        for (ll i = 1; g <= n; i++) {
+            g = lcm(g, i);
+            if (g > n) break;
+            ans += n / g;
+        }
+
+        cout << (ans+n)%MOD << '\n';
+    }    
+    
+    return 0;
+}
+
+
