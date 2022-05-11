4858e619668d7cf0a5537bda81aaffbcd2b09a9f
torontofangirl <toronto.tristan@gmail.com>
2022-05-11 04:42:47 -0500

    AS allSubset

--- AlphaStar/allSubset.cpp
+++ AlphaStar/allSubset.cpp
@@ -8,6 +8,14 @@ using pll = pair<long long, long long>;
 #define IN(A, B, C) assert(B <= A && A <= C)
 template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
 template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
 
 constexpr int MOD = 1e9 + 7;
 constexpr int INF = 1e9;
@@ -18,13 +26,20 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    int T; cin >> T; while (T--) {
-        ll n, m; cin >> n >> m;         
-        vector<ll> a(n);
-        for (auto& x : a) cin >> x;
+    ll n; cin >> n;
+    vector<ll> a(n);
+    for (auto& x : a) cin >> x;
+    
+    vector<ll> b;
+    y_combinator([&](auto rec, ll used, ll idx) -> void {
+        cout << b << endl;
+        if (idx == n) return;
 
-
-    }    
+        b.push_back(a[idx]);
+        rec(used+1, idx+1);
+        b.pop_back();
+        rec(used, idx+1);
+    })(0, 0);
     
     return 0;
 }
