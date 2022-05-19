9499a9c407ce844d6ccec3db5401227bb828e807
torontofangirl <toronto.tristan@gmail.com>
2022-05-19 05:27:02 -0500

    G302977F

---
+++ Codeforces/G302977F.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<long long, long long>;
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define IN(A, B, C) assert(B <= A && A <= C)
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+constexpr int MOD = 1e9 + 7;
+constexpr int INF = 1e9;
+constexpr ll INFF = 1e18;
+
+
+__int128 read() {
+    __int128 x = 0, f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9') {
+        if (ch == '-') f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9') {
+        x = x * 10 + ch - '0';
+        ch = getchar();
+    }
+    return x * f;
+}
+void print(__int128 x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) print(x / 10);
+    putchar(x % 10 + '0');
+}
+bool cmp(__int128 x, __int128 y) { return x > y; }
+
+int C(int n, int k) {
+    double res = 1;
+    for (int i = 1; i <= k; ++i)
+        res = res * (n - k + i) / i;
+    return (int)(res + 0.01);
+}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    ll n, k; while (cin >> n >> k) {
+        ll ans = 1;
+
+        //number of ways to choose, and number of ways
+        //to be out of order (derangements)
+        if (k >= 2) ans += 1ll*n*(n-1)/2;
+        if (k >= 3) ans += 1ll*n*(n-1)*(n-2)/3;
+        if (k >= 4) ans += 3ll*n*(n-1)*(n-2)*(n-3)/8;
+
+        cout << ans << endl;
+    }
+    
+    return 0;
+}
+
