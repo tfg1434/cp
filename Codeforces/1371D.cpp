332f03153629c737ddbcb3f56660c906aa0fe126
torontofangirl <toronto.tristan@gmail.com>
2022-05-20 07:19:45 -0500

    1371D

---
+++ Codeforces/1371D.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<long long, long long>;
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define IN(A, B, C) assert(B <= A && A <= C)
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+constexpr int MOD = 1e9 + 7;
+constexpr int INF = 1e9;
+constexpr ll INFF = 1e18;
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int T; cin >> T; while (T--) {
+        ll n, k; cin >> n >> k;
+
+        if (k % n == 0) cout << 0 << endl;
+        else cout << 2 << endl;
+
+        vector<vector<ll>> a(n, vector<ll>(n, 0));
+        ll r = 0, c = 0;
+        while (k > 0) {
+            k--;
+            a[r][c] = 1;
+            r++; c++; c %= n;
+
+            if (r == n) {
+                r = 0; c++; c %= n;
+            }
+        }
+
+        for (ll i = 0; i < n; i++) {
+            for (ll j = 0; j < n; j++) {
+                cout << a[i][j];
+            }
+            cout << '\n';
+        }
+    }    
+    
+    return 0;
+}
+
