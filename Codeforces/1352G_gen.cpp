5a43012ae4a578f3edbb493a63e0f56b5810197b
torontofangirl <toronto.tristan@gmail.com>
2022-05-22 09:21:53 -0500

    1352G

---
+++ Codeforces/1352G_gen.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<long long, long long>;
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define IN(A, B, C) assert(B <= A && A <= C)
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+constexpr int MOD = 1e9 + 7;
+constexpr int INF = 1e9;
+constexpr ll INFF = 1e18;
+
+
+int rand(int a, int b) {
+    return a + rand() % (b - a + 1);
+}
+
+int main(int argc, char *argv[]) {
+    ll seed = atoi(argv[1]);
+    srand(seed);
+    ll n = rand(1, 7);
+    cout << 1 << endl << n << endl;
+    
+    return 0;
+}
+
