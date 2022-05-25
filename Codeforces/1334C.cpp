a4106b2104d923b01e4d4fc3e972e30b6ecc4bcd
torontofangirl <toronto.tristan@gmail.com>
2022-05-25 07:01:02 -0500

    1334C

---
+++ Codeforces/1334C.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<long long, long long>;
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define IN(A, B, C) assert(B <= A && A <= C)
+#define mod2(a, n) ((a%n)+n)%n
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+constexpr int MOD = 1e9 + 7;
+constexpr int INF = 1e9;
+constexpr ll INFF = 1e18;
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int T; cin >> T; while (T--) {
+        ll n; cin >> n;
+        vector<pll> a(n);
+        for (auto &[x, y] : a) cin >> x >> y;
+
+        // cout << a << '\n';
+        ll w = 0;
+        for (ll i = 0; i < n; i++) {
+            w += max(0ll, a[i].first - a[mod2((i-1),n)].second);
+        }
+        
+        ll ans = INFF;
+        for (ll i = 0; i < n; i++) {
+            ll x = max(0ll, a[i].first - a[mod2((i-1),n)].second);
+            ans = min(ans, w - x + a[i].first);
+        }
+
+        cout << ans << '\n';
+    }    
+    
+    return 0;
+}
+
