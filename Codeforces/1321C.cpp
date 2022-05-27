f98ea4875e2ec5518164b7992f86b07a2fe1a7f9
torontofangirl <toronto.tristan@gmail.com>
2022-05-27 05:09:28 -0500

    1321C

---
+++ Codeforces/1321C.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<long long, long long>;
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define IN(A, B, C) assert(B <= A && A <= C)
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+constexpr int MOD = 1e9 + 7;
+constexpr int INF = 1e9;
+constexpr ll INFF = 1e18;
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    ll n; cin >> n;
+    string s; cin >> s;
+
+    ll ans = 0;
+    while (true) {
+        n = s.size();
+        ll maxc = 'a'-1, maxi = -1;
+        for (ll i = 0; i < n; i++) {
+            if (s[i] <= maxc) continue;
+
+            if (i > 0 && s[i] == s[i-1]+1) {
+                maxc = s[i];
+                maxi = i;
+                continue;
+            } else if (i < n-1 && s[i] == s[i+1]+1) {
+                maxc = s[i];
+                maxi = i;
+                continue;
+            }
+        }
+
+        if (maxi == -1) break;
+
+        s.erase(maxi, 1);
+        ans++;
+    }
+
+    cout << ans << endl;
+    
+    return 0;
+}
+
