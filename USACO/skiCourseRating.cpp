ec12260b903e848674a0edaba5bcf7967a273f02
torontofangirl <toronto.tristan@gmail.com>
2022-07-23 14:19:55 -0500

    no

--- USACO/skiCourseRating.cpp
+++ USACO/skiCourseRating.cpp
@@ -28,17 +28,35 @@ struct DSU {
 		return 1;
 	}
 };
+struct Node {
+    ll x, y;
+    Node* pa;
+    ll sz, wt, rank;
+};
+struct Edge {
+    Node *u, *v;
+    ll wt;
+    Edge() {}
+    Edge(Node *u, Node *v, ll wt) : u(u), v(v), wt(wt) {}
+};
 
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    ll m, n; cin >> m >> n;
+    ll m, n, t; cin >> m >> n >> t;
     vector<vector<ll>> e(m+1, vector<ll>(n+1));
+    vector<vector<Node>> a(m+1, vector<Node>(n+1));
     for (ll i = 1; i <= m; i++) {
         for (ll j = 1; j <= n; j++) {
             cin >> e[i][j];
+            a[i][j].x = i;
+            a[i][j].y = j;
+            a[i][j].pa = &a[i][j];
+            a[i][j].sz = 1;
+            a[i][j].wt = -1;
+            a[i][j].rank = 0;
         }
     }
     vector<vector<ll>> f(m+1, vector<ll>(n+1));
@@ -48,7 +66,23 @@ int main() {
         }
     }
 
-    DSU dsu; dsu.init(m*n);
+    DSU dsu; dsu.init(m*n+1);
+    vector<Edge> edges;
+    for (ll i = 1; i <= m; i++) {
+        for (ll j = 1; j <= n; j++) {
+            if (i < m) {
+                edges.push_back(Edge(&a[i][j], &a[i+1][j], abs(e[i+1][j]-e[i][j])));
+            }
+            if (j < n) {
+                edges.push_back(Edge(&a[i][j], &a[i][j+1], abs(e[i][j+1]-e[i][j])));
+            }
+        }
+    }
+    sort(all(edges), [](const Edge &a, const Edge &b) { return a.wt < b.wt; });
+    for (ll i = 0; i < len(edges); i++) {
+        Edge e = edges[i];
+        Node* u = no
+    }
 
     
     return 0;
