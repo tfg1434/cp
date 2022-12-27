14e3ff625ce7c6ab7bbb39ba2d42fa1fa687100f
torontofangirl <toronto.tristan@gmail.com>
2022-12-27 08:47:49 -0600

    PKU oulipo

---
+++ PKU/oulipo.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<long long, long long>;
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define len(a) (ll)((a).size())
+#define IN(A, B, C) assert(B <= A && A <= C)
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+
+
+constexpr int MOD = 1e9 + 7;
+constexpr int INF = 1e9;
+constexpr ll INFF = 1e18;
+
+
+vector<ll> getF(string p) {
+    ll m = p.size();
+    vector<ll> F(m);
+
+    for (ll i = 1, k = 0; i < m; i++) {
+        while (k > 0 && p[k] != p[i]) k = F[k-1];
+
+        if (F[k] == F[i]) F[i] = ++k;
+        else F[i] = k;
+    }
+
+    return F;
+}
+
+ll KMP(string s, string p) {
+    ll n = s.size(), m = p.size();
+    ll res = 0;
+    vector<ll> F = getF(p);
+
+    for (ll i =0, k=0; i < n; i++) {
+        while (k > 0 && p[k] != s[i]) k = F[k-1];
+
+        if (p[k] == s[i]) k++;
+
+        if (k == m) {
+            res++;
+            k = F[k-1];
+        }
+    }
+
+    return res;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int T; cin >> T; while (T--) {
+        string w, t; cin >> w >> t;
+        cout << KMP(t, w) << endl;
+    } 
+    
+    return 0;
+}
+
