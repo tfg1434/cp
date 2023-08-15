32563b0b4d712a6c8e4ff864e6ba0ab4f07440ad
torontofangirl <toronto.tristan@gmail.com>
2023-08-14 20:21:44 -0500

    rectangularPasture USA Implementation Olympiad

--- USACO/rectangularPasture.cpp
+++ USACO/rectangularPasture.cpp
@@ -34,32 +34,63 @@ constexpr ll INFF = 1e18;
 constexpr ll P = 1e9+7;
 // constexpr ll P = 998244353;
 
+bool ycmp(pll a, pll b) { return a.s < b.s; }
+
+// Thinking
+// Didn't think of edges, thought about corners, which is wrong
+// Tried to overcomplicate a silver problem, convexity, no
+// Thought about minimal principle
+
+// Implementation
+// Why 1-index? nightmare
+// Don't need map to coordinate compress. even easier here b/c 
+// all x, y distinct.
+// Draw your paper diagram with (0,0) in top left
 int main() {
     cin.tie(0) -> ios::sync_with_stdio(0);
     
     ll n; while (cin >> n) {
-        vector<pll> a(n+1);
-        f1(i, n+1) {
+        vector<pll> a(n);
+        f0(i, n) {
             cin >> a[i].f >> a[i].s;
         }
+        sort(all(a));
+        f0(i, n) a[i].f = i+1;
+        sort(all(a), ycmp);
+        f0(i, n) a[i].s = i+1;
 
-        vector<ll> tl(n+1), tr(n+1);
+        vector<vi> pre(n+1, vi(n+1));
+        f0(i, n) pre[a[i].f][a[i].s] = 1;
         f1(i, n+1) {
             f1(j, n+1) {
-                if (i == j) continue;
-                if (a[j].s < a[i].s) continue;
-                if (a[j].f < a[i].f) {
-                    tl[i]++;
-                } else {
-                    tr[i]++;
-                }
+                pre[i][j] += pre[i-1][j] + pre[i][j-1] - pre[i-1][j-1];
             }
         }
 
+        //inclusive sum x2, y2 BR corner
+        auto topsum = [&](ll x1, ll x2, ll y) -> ll {
+            return pre[x2][y] - pre[x1-1][y];
+        };
+        auto botsum = [&](ll x1, ll x2, ll y) -> ll {
+            // if (x1 == x2) gg(pre[x2][n] , pre[x1-1][n] , pre[x2][y-1] , pre[x1-1][y-1]);
+            return pre[x2][n] - pre[x1-1][n] - pre[x2][y-1] + pre[x1-1][y-1];
+        };
+
+        sort(all(a));
         ll ans = 0;
-        f1(i, n+1) {
-            ans += (tl[i]+1)*(tr[i]+1);
+        f0(i, n) {
+            for (ll j = i; j < n; j++) {
+                pll p1 = a[i], p2 = a[j];
+                assert(p1.f <= p2.f);
+                ll a = topsum(p1.f, p2.f, min(p1.s, p2.s));
+                ll b = botsum(p1.f, p2.f, max(p1.s, p2.s));
+                // gg(p1, p2);
+                // gg(a,b);
+                ans += a*b;
+            }
+            // gg(ans);
         }
+
         cout << ans+1 << endl;
     }
     
