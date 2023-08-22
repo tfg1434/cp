8048fe75a97a9bc9aca317b3575ddce5720efaf6
torontofangirl <toronto.tristan@gmail.com>
2023-08-21 20:47:28 -0500

    USACO Robot Instructions

---
+++ USACO/robotInstructions.cpp
@@ -0,0 +1,111 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+using namespace __gnu_pbds;
+#include <bits/stdc++.h>
+using namespace std;
+using pll = pair<long long, long long>;
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define eb emplace_back
+#define vi vector<ll>
+#define vb vector<bool>
+#define f0(i,a) for(ll i=0;i<(a);i++)
+#define f1(i,a) for(ll i=1;i<(a);i++)
+#define rep(i,a,b) for(int i=(a);i<=(b);++i)
+#define per(i,a,b) for(int i=(a);i>=(b);--i)
+#define f first
+#define s second
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+#ifdef I_AM_NOOB
+#define gg(...) [](const auto&...x){ char c='='; cerr<<#__VA_ARGS__; ((cerr<<exchange(c,',')<<x),...); cerr<<endl; }(__VA_ARGS__);
+#else
+#define gg(...) 777771449
+#endif
+ll LOG2(ll x){ return __builtin_clzll(1ll) - __builtin_clzll(x); }
+bool isPow2(ll n) {
+    return n && ((n & (n-1)) == 0);
+}
+ll LOG2C(ll x) {
+    if (isPow2(x)) return LOG2(x);
+    return LOG2(x)+1;
+}
+template <typename Iter, typename Cont>
+bool is_last(Iter iter, const Cont& cont) {
+    return (iter != cont.end()) && (next(iter) == cont.end());
+}
+
+constexpr ll INFF = 1e18;
+constexpr ll P = 1e9+7;
+// constexpr ll P = 998244353;
+
+struct CHash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+    int operator()(pll x) const { return x.f * 31+x.s; }
+};
+
+// Very standard meet in the middle
+int main() {
+    cin.tie(0) -> ios::sync_with_stdio(0);
+    
+    ll n; while (cin >> n) {
+        ll xg, yg; cin >> xg >> yg;
+        vi xs(n), ys(n);
+        f0(i, n) {
+            cin >> xs[i] >> ys[i];
+        }
+        
+        vector<gp_hash_table<pll, ll, CHash>> mp(n+1);
+        f0(i, (1 << n/2)) {
+            ll x = 0, y = 0;
+            f0(j, n/2) {
+                if (i & (1 << j)) {
+                    x += xs[j];
+                    y += ys[j];
+                }
+            }
+            mp[__builtin_popcount(i)][{x, y}]++;
+        }
+        
+        vi ans(n+1);
+        f0(i, (1 << (n+1)/2)) {
+            ll x = xg, y = yg;
+            f0(j, (n+1)/2) {
+                if (i & (1 << j)) {
+                    x -= xs[n/2+j];
+                    y -= ys[n/2+j];
+                }
+            }
+
+            ll b = __builtin_popcount(i);
+            f0(j, n+1) {
+                ans[j+b] += mp[j][{x, y}];
+            }
+        }
+
+        f1(i, n+1) cout << ans[i] << endl;
+    } 
+    
+    return 0;
+}
