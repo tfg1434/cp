6bbe83b0c135e96d14bd463a766816b8e0cac5d4
torontofangirl <toronto.tristan@gmail.com>
2023-08-23 22:16:06 -0500

    JOI22_ho_t2

---
+++ ojuz/JOI22_ho_t2.cpp
@@ -0,0 +1,93 @@
+#pragma GCC optimize "trapv"
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+using namespace __gnu_pbds;
+#include <bits/stdc++.h>
+using namespace std;
+using pll = pair<long long, long long>;
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define eb emplace_back
+#define vi vector<ll>
+#define vb vector<bool>
+#define f0(i,a) for(ll i=0;i<(a);i++)
+#define f1(i,a) for(ll i=1;i<(a);i++)
+#define rep(i,a,b) for(int i=(a);i<=(b);++i)
+#define per(i,a,b) for(int i=(a);i>=(b);--i)
+#define f first
+#define s second
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+#ifdef I_AM_NOOB
+#define gg(...) [](const auto&...x){ char c='='; cerr<<#__VA_ARGS__; ((cerr<<exchange(c,',')<<x),...); cerr<<endl; }(__VA_ARGS__);
+#else
+#define gg(...) 777771449
+#endif
+ll LOG2(ll x){ return __builtin_clzll(1ll) - __builtin_clzll(x); }
+bool isPow2(ll n) {
+    return n && ((n & (n-1)) == 0);
+}
+ll LOG2C(ll x) {
+    if (isPow2(x)) return LOG2(x);
+    return LOG2(x)+1;
+}
+template <typename Iter, typename Cont>
+bool is_last(Iter iter, const Cont& cont) {
+    return (iter != cont.end()) && (next(iter) == cont.end());
+}
+
+constexpr ll INFF = 1e18;
+constexpr ll P = 1e9+7;
+// constexpr ll P = 998244353;
+
+// Thinking
+// Easy 
+//
+// Implementation
+// Made an overflow error which took long time to debug...
+// #pragma GCC optimize "trapv"
+int main() {
+    cin.tie(0) -> ios::sync_with_stdio(0);
+    
+    ll n, m; while (cin >> n >> m) {
+        vi a(n), b(n);
+        for (auto&x:a) cin >> x;
+        for (auto&x:b) cin >> x;
+        // f0(i, n) if (a[i] < b[i]) a[i] = b[i];
+
+        auto check = [&](ll x) -> bool {
+            ll days = n*m;
+            f0(i, n) {
+                ll c = x;
+                if (a[i] > b[i]) {
+                    ll d = min(m, (x + a[i]-1) / a[i]);
+                    days -= d;
+                    c -= a[i] * d;
+                }
+                if (c > 0) days -= (c + b[i] - 1) / b[i];
+                if (days < 0) return false;
+            }
+
+            return true;
+        };
+        ll K = 0, D = 1e18;
+        while (D) {
+            while (check(K+D)) K += D;
+            D /= 2;
+        }
+
+        cout << K << endl;
+    } 
+    
+    return 0;
+}
