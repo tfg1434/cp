35b75e2c2987f61bbd2a335a88ce9cc3e1dcec79
torontofangirl <toronto.tristan@gmail.com>
2023-08-29 22:09:18 -0500

    loan repayment

---
+++ USACO/loanRepayment.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pll = pair<long long, long long>;
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define eb emplace_back
+#define vi vector<ll>
+#define vb vector<bool>
+#define f0(i,a) for(ll i=0;i<(a);i++)
+#define f1(i,a) for(ll i=1;i<(a);i++)
+#define rep(i,a,b) for(int i=(a);i<=(b);++i)
+#define per(i,a,b) for(int i=(a);i>=(b);--i)
+#define f first
+#define s second
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+#ifdef I_AM_NOOB
+#define gg(...) [](const auto&...x){ char c='='; cerr<<#__VA_ARGS__; ((cerr<<exchange(c,',')<<x),...); cerr<<endl; }(__VA_ARGS__);
+#else
+#define gg(...) 777771449
+#endif
+
+constexpr ll INFF = 1e18;
+constexpr ll P = 1e9+7;
+// constexpr ll P = 998244353;
+
+double binpow(double a, long long b) {
+    if (b == 0)
+        return 1;
+    long long res = binpow(a, b / 2);
+    if (b % 2)
+        return res * res * a;
+    else
+        return res * res;
+}
+
+int main() {
+    cin.tie(0) -> ios::sync_with_stdio(0);
+    freopen("loan.in", "r", stdin);
+    freopen("loan.out", "w", stdout);
+    
+    ll n, k, m; while (cin >> n >> k >> m) {
+        auto check = [&](ll x) -> bool {
+            ll g = 0, kk = k;
+            while (kk > 0 && g < n) {
+                ll y = (n-g)/x;
+                if (y < m) {
+                    ll rem = (n-g+m-1) / m;
+                    return rem <= kk;
+                }
+                
+                ll mx = n-x*y;
+                ll d = (mx - g) / y + 1;
+                d = min(d, kk);
+                g += y * d;
+                kk -= d;
+                gg(kk);
+            }
+
+            return g >= n;
+        };
+
+        ll p = 0;
+        for (ll a = n; a >= 1; a /= 2) {
+            while (check(p+a)) p += a;
+        }
+
+        cout << p << endl;
+    } 
+    
+    return 0;
+}
