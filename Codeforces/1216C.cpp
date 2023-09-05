58353c0dac46e25b9b4a7ff97d2def5f9ed12ba2
torontofangirl <toronto.tristan@gmail.com>
2023-09-04 22:57:08 -0500

    1216C

--- Codeforces/1216C.cpp
+++ Codeforces/1216C.cpp
@@ -13,8 +13,10 @@ struct Rect{
     
     [[nodiscard]]
     ll intersect(Rect other) const {
-        ll xLap = max(0ll, min(x2, other.x2) - max(x1, other.x1));
-        ll yLap = max(0ll, min(y2, other.y2) - max(y1, other.y1));
+        ll xLap = max(0ll, other.x1 - x2);
+        ll yLap = max(0ll, other.y1 - y2);
+        // ll xLap = max(0ll, min(x2, other.x2) - max(x1, other.x1));
+        // ll yLap = max(0ll, min(y2, other.y2) - max(y1, other.y1));
         
         return xLap * yLap;
     }
@@ -24,22 +26,21 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    Rect white, a, b;
-    cin >> white.x1 >> white.y1 >> white.x2 >> white.y2;
-    cin >> a.x1 >> a.y1 >> a.x2 >> a.y2;
-    cin >> b.x1 >> b.y1 >> b.x2 >> b.y2;
-    
-    ll total = white.intersect(a) + white.intersect(b) - a.intersect(b);
-    if (total >= white.area())
-        cout << "NO" << '\n';
-    else
-        cout << "YES" << '\n';
-    
-//    if (white.hasIntersect(a) || white.hasIntersect(b))
-//        cout << "NO" << '\n';
-//    else
-//        cout << "YES" << '\n';
-    
+    ll x1, y1, x2, y2, x3, y3, x4, y4;
+    cin>>x1>>y1>>x2>>y2;
+    for(ll i=1; i<=2; i++){
+        cin>>x3>>y3>>x4>>y4;
+        if(x1>=x3 && y1>=y3 && x4>=x2 && y4>=y2){
+            cout<<"NO"; 
+            return 0;
+        }
+        if(x1>x4 || x3>x2 || y3>y2 || y4<y1) continue;
+        if(x1>=x3 && y1>=y3 && x4>=x2)y1=y4;
+        if(x1>=x3 && y1>=y3 && y4>=y2)x1=x4;
+        if(x1>=x3 && y4>=y2 && x4>=x2)y2=y3;
+        if(x4>=x2 && y1>=y3 && y4>=y2)x2=x3;
+    }
+    cout<<"YES"<<endl;
     return 0;
 }
 
