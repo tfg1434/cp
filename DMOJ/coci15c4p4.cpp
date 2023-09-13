4dce348bb3590f237e61e83a5a8617d04ecdd9b2
torontofangirl <toronto.tristan@gmail.com>
2023-09-12 20:14:34 -0500

    coci15c4p4 chewbacca

---
+++ DMOJ/coci15c4p4.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pll = pair<long long, long long>;
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define eb emplace_back
+#define vi vector<ll>
+#define vb vector<bool>
+#define f0(i,a) for(ll i=0;i<(a);i++)
+#define f1(i,a) for(ll i=1;i<(a);i++)
+#define rep(i,a,b) for(int i=(a);i<=(b);++i)
+#define per(i,a,b) for(int i=(a);i>=(b);--i)
+#define f first
+#define s second
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+#ifdef I_AM_NOOB
+#define gg(...) [](const auto&...x){ char c='='; cerr<<#__VA_ARGS__; ((cerr<<exchange(c,',')<<x),...); cerr<<endl; }(__VA_ARGS__);
+#else
+#define gg(...) 777771449
+#endif
+
+constexpr ll INFF = 1e18;
+constexpr ll P = 1e9+7;
+// constexpr ll P = 998244353;
+ll binpow(ll a, ll b) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = res * a;
+        b >>= 1;
+        a = a * a;
+    }
+
+    return res;
+}
+
+int main() {
+    cin.tie(0) -> ios::sync_with_stdio(0);
+    
+    ll n, k, q; while (cin >> n >> k >> q) {
+        ll cur = 1, p = 1;
+        vi a; a.pb(0);
+        while (cur <= k*n) {
+            a.pb(cur);
+            p *= k;
+            cur += p;
+        }
+        f0(i, q) {
+            ll ans = 0;
+            ll x, y; cin >> x >> y;
+            if (x > y) swap(x, y);
+            auto la = prev(lower_bound(all(a), x));
+            auto lb = prev(lower_bound(all(a), y));
+            //Scale x to 0..*la-1
+            x -= *la+1;
+            y -= *lb+1;
+            ll posA = y;
+            if (la < lb) {
+                ll nA = la - a.begin();
+                ll nB = lb - a.begin();
+                ll d = nB - nA;
+                posA /= binpow(k, d); // floor(yy/*lb * la). well dividing lb by la is always clean
+                ans += d;
+            }
+            
+            while (x != posA) {
+                x /= k;
+                posA /= k;
+                ans += 2;
+            }
+
+            cout << ans << endl;
+        }
+    } 
+    
+    return 0;
+}
+
