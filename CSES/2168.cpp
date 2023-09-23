5cc44c8c84080afff3b8a134d4e2cb1e8619b609
torontofangirl <toronto.tristan@gmail.com>
2023-09-23 16:54:45 -0500

    class

---
+++ CSES/2168.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pl = pair<long long, long long>;
+using str = string;
+using db = long double;
+#define ll long long
+using pd = pair<db, db>;
+
+#define tcT template<class T
+#define tcTU tcT, class U
+// ^ lol this makes everything look weird but I'll try it
+tcT> using V = vector<T>; 
+tcT, size_t SZ> using AR = array<T,SZ>; 
+using vb = V<bool>;
+using vl = V<ll>;
+using vd = V<db>;
+using vs = V<str>;
+using vpl = V<pl>;
+using vpd = V<pd>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define eb emplace_back
+#define bk back()
+#define f0(i,a) for(ll i=0;i<(a);i++)
+#define f1(i,a) for(ll i=1;i<(a);i++)
+#define rep(i,a,b) for(int i=(a);i<=(b);++i)
+#define per(i,a,b) for(int i=(a);i>=(b);--i)
+#define f first
+#define s second
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+#ifdef I_AM_NOOB
+#define gg(...) [](const auto&...x){ char c='='; cerr<<#__VA_ARGS__; ((cerr<<exchange(c,',')<<x),...); cerr<<endl; }(__VA_ARGS__);
+#else
+#define gg(...) 777771449
+#endif
+
+constexpr ll INFF = 1e18;
+constexpr ll P = 1e9+7;
+// constexpr ll P = 998244353;
+struct Segment {
+    ll x, y, p;
+};
+
+int main() {
+    cin.tie(0) -> ios::sync_with_stdio(0);
+    
+    ll n; while (cin >> n) {
+        vector<Segment> a(n);
+        ll i = 0;
+        for (auto&[x, y, p] : a) cin >> x >> y, p = i++;
+        sort(all(a), [](auto a, auto b) -> bool {
+            if (a.x == b.x) return a.y > b.y;
+            return a.x < b.x;
+        });
+        ll mnR = INFF;
+        vl ans1(n), ans2(n);
+        for (ll i = n-1; i >= 0; i--) {
+            if (mnR <= a[i].y) ans1[a[i].p] = 1;
+            mnR = min(mnR, a[i].y);
+        }
+
+        ll mxR = 0;
+        for (ll i = 0; i < n; i++) {
+            if (mxR >= a[i].y) ans2[a[i].p] = 1;
+            mxR = max(mxR, a[i].y);
+        }
+
+        for (auto x : ans1) cout << x << ' ';
+        cout << endl;
+        for (auto x : ans2) cout << x << ' ';
+        cout << endl;
+    } 
+    
+    return 0;
+}
---
+++ Codeforces/1873H.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pl = pair<long long, long long>;
+using str = string;
+using db = long double;
+#define ll long long
+using pd = pair<db, db>;
+
+#define tcT template<class T
+#define tcTU tcT, class U
+// ^ lol this makes everything look weird but I'll try it
+tcT> using V = vector<T>; 
+tcT, size_t SZ> using AR = array<T,SZ>; 
+using vb = V<bool>;
+using vl = V<ll>;
+using vd = V<db>;
+using vs = V<str>;
+using vpl = V<pl>;
+using vpd = V<pd>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define eb emplace_back
+#define bk back()
+#define f0(i,a) for(ll i=0;i<(a);i++)
+#define f1(i,a) for(ll i=1;i<(a);i++)
+#define rep(i,a,b) for(int i=(a);i<=(b);++i)
+#define per(i,a,b) for(int i=(a);i>=(b);--i)
+#define f first
+#define s second
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+#ifdef I_AM_NOOB
+#define gg(...) [](const auto&...x){ char c='='; cerr<<#__VA_ARGS__; ((cerr<<exchange(c,',')<<x),...); cerr<<endl; }(__VA_ARGS__);
+#else
+#define gg(...) 777771449
+#endif
+
+constexpr ll INFF = 1e18;
+constexpr ll P = 1e9+7;
+// constexpr ll P = 998244353;
+
+int main() {
+    cin.tie(0) -> ios::sync_with_stdio(0);
+    
+    int T; cin >> T; while (T--) {
+        ll n, a, b; cin >> n >> a >> b;
+        vector<vl> g(n+1);
+        f0(i, n) {
+            ll u, v; cin >> u >> v;
+            g[u].pb(v); g[v].pb(u);
+        }
+        vl vis(n+1), par(n+1);
+        vl cyc;
+        auto dfs = y_combinator([&](auto rec, ll u, ll p) -> void {
+            vis[u] = 1;
+            par[u] = p;
+            for (auto v : g[u]) if (p != v){
+                if (vis[v]) {
+                    ll x = u;
+                    cyc.pb(u);
+                    while (par[x]) {
+                        x = par[x];
+                        cyc.pb(x);
+                    }
+                } else {
+                    rec(v, u);
+                }
+            }
+        });
+        dfs(1, 0);
+
+        queue<ll> q;
+        for (auto t : cyc) q.push(t);
+        ll minDistA = INFF, minDistB = INFF;
+        ll dist = 0;
+        vis.clear();
+        while (!q.empty()) {
+            ll m = q.size();
+            f0(i, m) {
+                ll u = q.front(); q.pop();
+                vis[u] = true;
+                if (u == a) minDistA = min(minDistA, dist);
+                if (u == b) minDistB = dist;
+                for (auto v : g[u]) if (!vis[v]) q.push(v);
+            }
+            dist++;
+        }
+
+        if (minDistA >= minDistB) cout << "YES";
+        else cout << "NO";
+        cout << endl;
+    } 
+    
+    return 0;
+}
