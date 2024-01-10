ebcaf2f0233b094e2ff3108084acf240a6108531
torontofangirl <toronto.tristan@gmail.com>
2024-01-10 07:53:40 -0600

    Gold out of sorts

--- USACO/stress.sh
+++ USACO/stress.sh
@@ -1,17 +1,19 @@
 set -e
-g++ modernArtIII.cpp -std=c++17 -o code
-g++ gen.cpp -std=c++17 -o gen
-g++ brute.cpp -std=c++17 -o brute
-for((i = 1; ; ++i)); do
+g++ outofsort.cpp -std=c++17 -o code
+g++ sortgen.cpp -std=c++17 -o gen
+# g++ brute.cpp -std=c++17 -o brute
+for((i = 1; i < 100; ++i)); do
     ./gen $i > input_file
     ./code < input_file > myAnswer
-    ./brute < input_file > correctAnswer
-    cmp --silent myAnswer correctAnswer || break
-    echo "Passed test: "  $i
+    cat input_file
+    cat myAnswer
+    # cmp --silent myAnswer correctAnswer || break
+    # echo "Passed test: "  $i
 done
-echo "WA on the following test:"
-cat input_file
-echo "Your answer is:"
-cat myAnswer
-echo "Correct answer is:"
-cat correctAnswer
+
+# echo "WA on the following test:"
+# cat input_file
+# echo "Your answer is:"
+# cat myAnswer
+# echo "Correct answer is:"
+# cat correctAnswer
