c53e1e4326ad62b2f622307482a9cae64b33e5af
torontofangirl <toronto.tristan@gmail.com>
2024-02-20 11:12:28 -0600

    G_greedy_R wa??

---
+++ Codeforces/G_greedy_R.py
@@ -0,0 +1,16 @@
+def solve():
+    n = int(input())
+    a = list(map(int, input().split()))
+    a.sort()
+    a = list(reversed(a))
+
+    if n == 1:
+        print(a[0])
+        return
+
+    if a[0]-1 <= sum(a[1:]):
+        print(sum(a))
+    else: 
+        print(2*sum(a[1:])+1)
+
+solve()
