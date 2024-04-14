f5c07cbd2f50ed9170f7089a712d7b7af4980bff
torontofangirl <toronto.tristan@gmail.com>
2024-04-14 11:40:50 -0500

    SPOJ Merge sort tree practice KQUERYO

---
+++ SPOJ/KQUERYO.cpp
@@ -0,0 +1,237 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using db = long double; // or double, if TL is tight
+using str = string; // yay python! 
+
+// pairs
+using pi = pair<int,int>;
+using pl = pair<ll,ll>;
+using pd = pair<db,db>;
+#define mp make_pair
+#define f first
+#define s second
+
+#define tcT template<class T
+#define tcTU tcT, class U
+// ^ lol this makes everything look weird but I'll try it
+tcT> using V = vector<T>; 
+tcT, size_t SZ> using AR = array<T,SZ>; 
+using vi = V<int>;
+using vb = V<bool>;
+using vl = V<ll>;
+using vd = V<db>;
+using vs = V<str>;
+using vpi = V<pi>;
+using vpl = V<pl>;
+using vpd = V<pd>;
+
+// vectors
+// oops size(x), rbegin(x), rend(x) need C++17
+#define sz(x) ll((x).size())
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) x.rbegin(), x.rend() 
+#define sor(x) sort(all(x)) 
+#define rsz resize
+#define ins insert 
+#define pb push_back
+#define eb emplace_back
+#define ft front()
+#define bk back()
+
+#define lb lower_bound
+#define ub upper_bound
+tcT> ll lwb(V<T>& a, const T& b) { return ll(lb(all(a),b)-bg(a)); }
+tcT> ll upb(V<T>& a, const T& b) { return ll(ub(all(a),b)-bg(a)); }
+
+// loops
+#define FOR(i,a,b) for (ll i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define F1R(i,a) FOR(i,1,a+1)
+#define ROF(i,a,b) for (ll i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define R1F(i,a) ROF(i,1,a+1)
+#define rep(a) F0R(__________,a)
+#define each(a,x) for (auto& a: x)
+
+const ll P = 1e9+7; // 998244353;
+const ll MX = 2e5+5;
+const ll BIG = 1e18; // not too close to LLONG_MAX
+const db PI = acos((db)-1);
+const ll dx[4]{1,0,-1,0}, dy[4]{0,1,0,-1}; // for every grid problem!!
+mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count()); 
+template<class T> using pqg = priority_queue<T,vector<T>,greater<T>>;
+
+ll pw(ll a, ll b) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = res * a % P;
+        b >>= 1;
+        a = a * a % P;
+    }
+
+    return res;
+}
+
+// bitwise ops
+// also see https://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html
+constexpr ll pct(ll x) { return __builtin_popcount(x); } // # of bits set
+constexpr ll bits(ll x) { // assert(x >= 0); // make C++11 compatible until USACO updates ...
+    return x == 0 ? 0 : 31-__builtin_clz(x); } // floor(log2(x)) 
+constexpr ll p2(ll x) { return 1<<x; }
+constexpr ll msk2(ll x) { return p2(x)-1; }
+
+ll cdiv(ll a, ll b) { return a/b+((a^b)>0&&a%b); } // divide a by b rounded up
+ll fdiv(ll a, ll b) { return a/b-((a^b)<0&&a%b); } // divide a by b rounded down
+
+tcT> bool ckmin(T& a, const T& b) {
+    return b < a ? a = b, 1 : 0; } // set a = min(a,b)
+tcT> bool ckmax(T& a, const T& b) {
+    return a < b ? a = b, 1 : 0; } // set a = max(a,b)
+// m is max, M is second max
+tcT> T tmax(T x, T& m, T& M) {
+    if (x <= m) return ckmax(M, x);
+    return ckmax(m, x);
+}
+tcT> T tmin(T x, T& m, T& M) {
+    if (x >= m) return ckmin(M, x);
+    return ckmin(m, x);
+}
+
+tcTU> T fstTrue(T lo, T hi, U f) {
+    ++hi; assert(lo <= hi); // assuming f is increasing
+    while (lo < hi) { // find first index such that f is true 
+        T mid = lo+(hi-lo)/2;
+        f(mid) ? hi = mid : lo = mid+1; 
+    } 
+    return lo;
+}
+tcTU> T lstTrue(T lo, T hi, U f) {
+    --lo; assert(lo <= hi); // assuming f is decreasing
+    while (lo < hi) { // find first index such that f is true 
+        T mid = lo+(hi-lo+1)/2;
+        f(mid) ? lo = mid : hi = mid-1;
+    } 
+    return lo;
+}
+tcT> void UNIQUE(vector<T>& v) { // sort and remove duplicates
+    sort(all(v)); v.erase(unique(all(v)),end(v)); }
+tcTU> void safeErase(T& t, const U& u) { // don't erase
+    auto it = t.find(u); assert(it != end(t));
+    t.erase(it); } // element that doesn't exist from (multi)set
+
+#define tcTUU tcT, class ...U
+
+inline namespace FileIO {
+    void setIn(str s)  { freopen(s.c_str(),"r",stdin); }
+    void setOut(str s) { freopen(s.c_str(),"w",stdout); }
+    void setIO(str s = "") {
+        cin.tie(0)->sync_with_stdio(0); // unsync C / C++ I/O streams
+        // cin.exceptions(cin.failbit);
+        // throws exception when do smth illegal
+        // ex. try to read letter into int
+        if (sz(s) && fopen((s+".in").c_str(), "r")) setIn(s+".in"), setOut(s+".out"); // for old USACO
+    }
+}
+
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) yy(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+
+struct chash {
+    // any random-ish large odd number will do
+    const uint64_t C = uint64_t(2e18 * PI) + 71;
+    // random 32-bit number
+    const uint32_t RANDOM =
+                    chrono::steady_clock::now().time_since_epoch().count();
+    size_t operator()(uint64_t x) const {
+        // see https://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html
+        return __builtin_bswap64((x ^ RANDOM) * C);
+    }
+};
+template <class K, class V> using cmap = unordered_map<K, V, chash>;
+// example usage: cmap<int, int>
+
+struct node_info {
+    vl a;
+    node_info() {
+        a = vl();
+    }
+    node_info(ll x) {
+        a = {x};
+    }
+};
+
+V<node_info> t;
+
+node_info merge_node(const node_info& a, const node_info& b) {
+    node_info res;
+    res.a.resize(sz(a.a) + sz(b.a));
+    merge(all(a.a), all(b.a), bg(res.a));
+    return res;
+}
+
+void build(ll v, ll l, ll r, const vl& a) {
+    if (l == r-1) {
+        t[v] = node_info(a[l]);
+        return;
+    }
+    ll m = (l+r)/2;
+    build(2*v+1, l, m, a);
+    build(2*v+2, m, r, a);
+    t[v] = merge_node(t[2*v+1], t[2*v+2]);
+}
+
+ll count_gtk(ll v, ll l, ll r, ll L, ll R, ll k) {
+    if (r <= L || R <= l) return 0;
+    if (L <= l && r <= R) return r-l - upb(t[v].a, k);
+    ll m = (l+r)/2;
+    return count_gtk(2*v+1, l, m, L, R, k) +
+           count_gtk(2*v+2, m, r, L, R, k);
+}
+
+void init_tree(ll n, vl& a) {
+    ll size = 1;
+    while (size < 2*n) size *= 2;
+    t.resize(size);
+    build(0, 0, n, a);
+}
+
+void solve() {
+    ll ans = 0;
+    ll n; cin >> n;
+    vl a(n); for (ll i = 0; i < n; i++) cin >> a[i];
+    init_tree(n, a);
+
+    ll q; cin >> q;
+    rep(q) {
+        ll i, j, k; cin >> i >> j >> k;
+        i ^= ans; j ^= ans; k ^= ans;
+        j = min(n, j);
+        i = max(1ll, i);
+        if (i > j) {
+            ans = 0;
+        } else {
+            i--;
+            ans = count_gtk(0, 0, n, i, j, k);
+        }
+        cout << ans << endl;
+    }
+}
+
+signed main() {
+    setIO();
+    
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--) solve();
+
+    return 0;
+}
