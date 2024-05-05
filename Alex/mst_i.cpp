64bda4c8af5452168bc0cdce363b92fc5187d4b0
torontofangirl <toronto.tristan@gmail.com>
2024-05-05 17:26:19 -0500

    mst i wa

---
+++ Alex/mst_i.cpp
@@ -0,0 +1,285 @@
+// #pragma GCC optimize("Ofast,no-stack-protector")
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+// #pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+const int INF = 1e18; // 1e9
+const int P = 1e9+7; // 998244353;
+const double PI = acos(-1.0);
+
+using pi = pair<int,int>;
+
+#define f first
+#define s second
+template<class T> using V = vector<T>; 
+using vi = V<int>;
+using vb = V<bool>;
+using vs = V<string>;
+using vpi = V<pi>;
+
+#define sz(x) ((int)(x).size())
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) x.rbegin(), x.rend() 
+#define pb push_back
+#define eb emplace_back
+#define ft front()
+#define bk back()
+
+#define lb lower_bound
+#define ub upper_bound
+template<class T> int lwb(V<T>& a, const T& b) { return lb(all(a),b)-bg(a); }
+template<class T> int upb(V<T>& a, const T& b) { return ub(all(a),b)-bg(a); }
+
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define rep(a) F0R(_,a)
+
+mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count()); 
+template<class T> using pqg = priority_queue<T,vector<T>,greater<T>>;
+constexpr int pct(int x) { return __builtin_popcount(x); }
+constexpr int bits(int x) { return x == 0 ? 0 : 31-__builtin_clz(x); } // floor(log2(x)) 
+constexpr int p2(int x) { return 1<<x; }
+constexpr int msk2(int x) { return p2(x)-1; }
+
+template<class T> bool ckmin(T& a, const T& b) {
+    return b < a ? a = b, 1 : 0; } // set a = min(a,b)
+template<class T> bool ckmax(T& a, const T& b) {
+    return a < b ? a = b, 1 : 0; } // set a = max(a,b)
+
+int fdiv(int a, int b) { return a/b - ((a^b)<0 && a%b);}
+
+ostream& operator<<(ostream&out, __int128 x);
+template<class T, class U> T fstFalse(T lo, T hi, U f) {
+    int ___ = 0;
+    while (lo < hi) {
+        // cout << lo << " to " << hi << endl;
+        T m = fdiv(lo+hi, 2);
+        f(m) ? lo = m+1 : hi = m;
+        if (___++ > 50) break;
+    }
+    return hi;
+}
+
+template<class, class=void> struct is_iterable : false_type {};
+template<class T> struct is_iterable<T, void_t<decltype(begin(declval<T&>())), decltype(end(declval<T&>()))>> : true_type {};
+template<class T> constexpr bool is_iterable_v = is_iterable<T>::value;
+
+template<class, class = void> struct is_readable : false_type {};
+template<class T> struct is_readable<T, typename std::enable_if_t<is_same_v<decltype(cin >> declval<T&>()), istream&>>> : true_type {};
+template<class T> constexpr bool is_readable_v = is_readable<T>::value;
+
+template<class, class = void> struct is_printable : false_type {};
+template<class T> struct is_printable<T, typename std::enable_if_t<is_same_v<decltype(cout << declval<T>()), ostream&>>> : true_type {};
+template<class T> constexpr bool is_printable_v = is_printable<T>::value;
+
+#define def(t, args...) \
+    t args;             \
+    re(args);
+
+// Input
+template<class T> constexpr bool needs_input_v = !is_readable_v<T> && is_iterable_v<T>;
+template<class T> typename enable_if<is_readable_v<T>,void>::type re(T& x) { cin >> x; } 
+template<class T, class... U> void re(T& t, U&... u);
+template<class T> void re(complex<T>& c) { T a,b; re(a,b); c = {a,b}; } 
+template<class T, class U> void re(pair<T,U>& p) { re(p.f,p.s); }
+template<class T> typename enable_if<needs_input_v<T>,void>::type re(T& i) { for(auto&x:i)re(x); }
+template<class T, class... U> void re(T& t, U&... u) { re(t); re(u...); }
+
+// String representation for printing
+template<class T> constexpr bool needs_output_v = !is_printable_v<T> && is_iterable_v<T>;
+template<class T> typename enable_if<is_printable_v<T>,string>::type ts(T v) {
+    stringstream ss; ss << fixed << setprecision(15) << v;
+    return ss.str(); 
+}
+template<class T> string bit_vec(T t) { // bit vector to string
+    string res = "{"; for (const auto& x : t) res += ts(x);
+    res += "}"; return res; 
+}
+string ts(V<bool> v) { return bit_vec(v); }
+template<size_t SZ> string ts(bitset<SZ> b) { return bit_vec(b); }
+
+template<class T, class U> string ts(pair<T, U> p);
+template<class T> typename enable_if<needs_output_v<T>,string>::type ts(T v);
+
+template<class T, class U> string ts(pair<T,U> p) { return "("+ts(p.f)+", "+ts(p.s)+")"; }
+template<class T> typename enable_if<is_iterable_v<T>,string>::type ts_sep(T v, string sep) {
+    bool fst = 1; string res = "";
+    for (const auto& x: v) {
+        if (!fst) res += sep;
+        fst = 0; res += ts(x);
+    }
+    return res;
+}
+template<class T> typename enable_if<needs_output_v<T>,string>::type ts(T v) { return "{"+ts_sep(v,", ")+"}"; }
+
+// Printing functions
+template<class T> void pr_sep(ostream& os, string, const T& t) { os << ts(t); }
+template<class T, class... U> void pr_sep(ostream& os, string sep, const T& t, const U&... u) {
+    pr_sep(os,sep,t); os << sep; pr_sep(os,sep,u...); }
+
+template<class ...T> void pr(const T&... t) { pr_sep(cout,"",t...); } 
+void ps() { cout << "\n"; }
+template<class ...T> void ps(const T&... t) { pr_sep(cout," ",t...); ps(); } 
+
+template<class ...T> void dbg_out(const T&... t) {
+    pr_sep(cerr," | ",t...); cerr << endl; }
+    void loc_info(int line, string names) {
+        cerr << "Line(" << line << ") -> [" << names << "]: "; }
+#ifdef I_AM_NOOB
+#define dbg(...) loc_info(__LINE__,#__VA_ARGS__), dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+// Fast IO
+void setIn(string s)  { freopen(s.c_str(),"r",stdin); }
+void setOut(string s) { freopen(s.c_str(),"w",stdout); }
+void setIO(string s = "") {
+    cin.tie(0)->sync_with_stdio(0);
+    if (sz(s) && fopen((s+".in").c_str(), "r")) setIn(s+".in"), setOut(s+".out"); // for old USACO
+}
+
+template<class F> struct y_combinator_result {
+    F f;
+    template<class T> explicit y_combinator_result(T &&f): f(std::forward<T>(f)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return f(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) yy(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+
+ostream& operator << (ostream&out, __int128 num) {
+    if (num == 0) return out << '0';
+
+    if (num < 0) {
+        out << '-';
+        num *= -1;
+    }
+    string res;
+    while (num > 0) {
+        res += (char)(num%10) + '0'; num /= 10;
+    }
+    reverse(all(res));
+
+    return out << res;
+}
+
+const int max_n = 5e4;
+int e[max_n];
+int find(int u) {
+    return e[u] < 0 ? u : e[u] = find(e[u]);
+}
+bool unite(int u, int v) {
+    if ((u=find(u)) != (v=find(v))) {
+        if (e[u] > e[v]) swap(u, v);
+        e[u] += e[v];
+        e[v] = u;
+        return true;
+    }
+    return false;
+}
+
+void solve() {
+    def(int, n, m, k);
+    if (m == 0) {
+        ps(0);
+        return;
+    }
+
+    vi black, red;
+    V<array<int, 4>> edges;
+    for (int i = 0; i < m; i++) {
+        def(int, u, v, w); u--; v--;
+        if (!u || !v) {
+            red.pb(i);
+            edges.pb({w, u, v, 1});
+        } else {
+            black.pb(i);
+            edges.pb({w, u, v, 0});
+        }
+    }
+
+    if (sz(red) && k == 0) { // too many red edges
+        ps(-1);
+        return;
+    }
+
+    auto cmp_wi = [&](int x, int y) {
+        if (edges[x][0] != edges[y][0]) return edges[x][0] < edges[y][0];
+        assert(x != y);
+        return x < y;
+    };
+    sort(all(red), cmp_wi);
+    sort(all(black), cmp_wi);
+
+    auto cmp = [&](int c1, int c2, int c3, int x, int y) {
+        auto w = edges[x][0] + c1*edges[x][3];
+        auto W = edges[y][0] + c1*edges[y][3];
+        if (w != W) return w < W;
+        auto i2 = x*x+c2*edges[x][3];
+        auto I2 = y*y+c2*edges[y][3];
+        if (i2 != I2) return i2 < I2;
+        auto i = x+c3*edges[x][3];
+        auto I = y+c3*edges[y][3];
+        if (i != I) return i < I;
+        assert(x != y);
+        return x < y;
+    };
+
+    __int128 hi = (__int128)m*m*m*m + m*m*m + m;
+    __int128 lo = -hi;
+
+    vi sol;
+    auto check = [&](__int128 pivot) {
+        memset(e, -1, sizeof e);
+        sol.clear();
+        int cnt_red = 0;
+
+        int c3 = pivot % m; pivot /= m;
+        int c2 = pivot % (m*m); pivot /= m*m;
+        int c1 = pivot;
+
+        int i = 0, j = 0;
+        while (i < sz(black) || j < sz(red)) {
+            if (i < sz(black) && (j >= sz(red) || cmp(c1, c2, c3, black[i], red[j]))) {
+                auto [w, u, v, _] = edges[black[i]];
+                if (unite(u, v)) {
+                    sol.pb(black[i]);
+                }
+                i++;
+            } else {
+                auto [w, u, v, _] = edges[red[j]];
+                if (unite(u, v)) {
+                    cnt_red++; 
+                    sol.pb(red[j]);
+                }
+                j++;
+            }
+        }
+
+        return cnt_red;
+    };
+    // for (__int128 i = lo; i < hi; i++) {
+        // ps(i, check(i));
+    // }
+    auto Pivot = fstFalse(lo, hi, [&](__int128 pivot) {return check(pivot) >= k;});
+    if (Pivot == lo) { // not enough red edges
+        ps(-1);
+        return;
+    }
+    Pivot--;
+    check(Pivot);
+    ps(sz(sol));
+    for (int i = 0; i < sz(sol); i++) cout << sol[i]+1 << " \n"[i==sz(sol)-1];
+}
+
+signed main() {
+    setIO();
+    
+    int tc = 1;
+    // cin >> tc;
+    while (tc--) solve();
+
+    return 0;
+}
