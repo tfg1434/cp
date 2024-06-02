5fb5488ec97c84f42ac6997d26f1097c5ed8715a
torontofangirl <toronto.tristan@gmail.com>
2024-06-02 17:02:52 -0500

    1981D wa but just something small

--- Codeforces/1981D.cpp
+++ Codeforces/1981D.cpp
@@ -1,3 +1,6 @@
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx2")
+
 #include <bits/stdc++.h>
 using namespace std;
 #define int int64_t
@@ -20,7 +23,6 @@ template<class T> int lwb(V<T>& a, const T& b) { return lb(all(a),b)-begin(a); }
 template<class T> int upb(V<T>& a, const T& b) { return ub(all(a),b)-begin(a); }
 template<class T> bool ckmin(T& a, const T& b) { return a > b ? a=b, true : false; }
 template<class T> bool ckmax(T& a, const T& b) { return a < b ? a=b, true : false; }
-constexpr int bits(int x) { return x == 0 ? 0 : 31-__builtin_clz(x); } // floor(log2(x)) 
 
 constexpr int MAX_SIEVE = 1e6;
 vi primes;
@@ -31,33 +33,72 @@ void sieve() {
             for (int j = i*i; j <= MAX_SIEVE; j += i)
                 isPrime[j] = false;
     }
-    primes.pb(1);
     for (int i = 2; i <= MAX_SIEVE; i++) if (isPrime[i])
         primes.pb(i);
 }
+int max_pairs(int k) {
+    // if (k % 2) return k*(k-1)/2;
+    // return k*(k-2)/2 + 1;
+    if (k % 2) return k*(k+1)/2;
+    return k*(k-2)/2 + 1 + k;
+}
+V<V<bool>> g;
 
 void solve() {
     int n; cin >> n;
-    if (n == 2) {
-        cout << "1 1\n";
-        return;
+    int lo = 1, hi = n;
+    while (hi-lo > 1) {
+        int mi = (lo+hi)/2;
+        max_pairs(mi) < n-1 ? lo = mi : hi = mi;
     }
-    int cnt = 0;
-    while (cnt*cnt < n-1) cnt++;
 
-    vi a(n);
-    V<V<bool>> used(cnt, V<bool>(cnt));
-    for (int i = 1; i < n; i++) {
-        for (int j = 0; j < cnt; j++) if (!used[a[i-1]][j]) {
-            used[a[i-1]][j] = true;
-            used[j][a[i-1]] = true;
-            a[i] = j;
-            break;
+    g.assign(hi, V<bool>(hi));
+    for (int i = 0; i < hi; i++) for (int j = 0; j < hi; j++) 
+        if (i != j) g[i][j] = g[j][i] = true;
+    if (hi % 2 == 0)
+        for (int i = 2; i < hi; i += 2) g[i][i+1] = g[i+1][i] = false;
+    vi nxt(hi);
+    for (int i = 0; i < hi; i++) while (!g[i][nxt[i]]) nxt[i]++;
+
+    vi s, epath;
+    s.pb(0);
+    while (s.size()) {
+        int u = s.back(); 
+
+        // int v;
+        // for (v = 0; v < hi; v++) {
+            // if (g[u][v]) break;
+        // }
+        // if (v == hi) {
+            // epath.pb(u);
+            // s.pop_back();
+        // } else {
+            // s.pb(v);
+            // g[u][v] = g[v][u] = false;
+        // }
+        // continue;
+
+        if (nxt[u] == hi) {
+            epath.pb(u);
+            s.pop_back();
+        } else {
+            s.pb(nxt[u]);
+            g[u][nxt[u]] = g[nxt[u]][u] = false;
+            while (nxt[u] < hi && !g[u][nxt[u]]) nxt[u]++;
         }
     }
-
-    for (int i = 0; i < n; i++) a[i] = primes[a[i]];
-    for (int i = 0; i < n; i++) cout << a[i] << " \n"[i==n-1];
+    
+    V<bool> used(hi);
+    vi ans;
+    for (int u : epath) {
+        ans.pb(u);
+        if (!used[u]) {
+            used[u] = true;
+            ans.pb(u);
+        }
+    }
+    for (int i = 0; i < n; i++) 
+        cout << primes[ans[i]] << " \n"[i==n-1];
 }
 
 signed main() {
