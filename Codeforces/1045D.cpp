14006cee5d01c5fc70eaa83468d4395e2cd91156
torontofangirl <toronto.tristan@gmail.com>
2024-06-05 17:17:53 -0500

    1045D

--- Codeforces/1045D.cpp
+++ Codeforces/1045D.cpp
@@ -1,6 +1,13 @@
+// Reading doubles can be extremely slow because c++ is initializing some buffers
+// or doing some other behind-the-scenes stuff; this was the source of TLE
+// scanf/printf can be faster in some cases, but you need to change everything
+// or it has a null (or negative) impact
+// Remove fastIO before scanf, use %d for int, %lf for double
+// setprecision will round to the nearest value
+
 #include <bits/stdc++.h>
 using namespace std;
-using db = long double;
+using db = double;
 
 #ifdef LOCAL
 #include "algo/debug.h"
@@ -23,52 +30,48 @@ template<class T> bool ckmax(T& a, const T& b) { return a < b ? a=b, true : fals
 
 const int max_n = 1e5+1;
 int par[max_n], n, q;
-db cont[max_n], p[max_n], sum;
+db sum_sons[max_n], p[max_n], sum;
 vi g[max_n];
 
 void dfs(int u, int fa) {
     par[u] = fa;
     for (int v : g[u]) if (v != fa) {
         dfs(v, u);
-        cont[u] += 1-p[v];
+        sum_sons[u] += 1-p[v];
     }
-    cont[u] *= p[u];
 }
 
 void solve() {
-    cin >> n;
-    for (int i = 0; i < n; i++) cin >> p[i];
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) scanf("%lf", &p[i]);
     for (int i = 0; i < n-1; i++) {
-        int u, v; cin >> u >> v;
+        int u, v; scanf("%d%d", &u, &v);
         g[u].pb(v); g[v].pb(u);
     }
-    g[0].pb(n); g[n].pb(0);
-    p[n] = 1;
-    dfs(n, -1);
-    for (int i = 0; i <= n; i++) sum += cont[i];
+    n++;
+    g[0].pb(n-1); g[n-1].pb(0); 
+    p[n-1] = 1;
+    dfs(n-1, -1);
+    for (int i = 0; i < n; i++) sum += p[i]*sum_sons[i];
 
-    cin >> q;
+    int q; scanf("%d", &q);
     while (q--) {
-        int u; db new_p; cin >> u >> new_p;
-        db delta = new_p - p[u];
+        int u; db new_p; 
+        scanf("%d%lf", &u, &new_p);
 
-        sum += p[par[u]]*(p[u]-new_p);
-        cont[par[u]] += p[par[u]]*(p[u]-new_p);
-        auto old_cont = cont[u];
-        if (p[u] > 0) cont[u] *= new_p / p[u];
-        sum += cont[u]-old_cont;
-
+        sum -= p[par[u]]*sum_sons[par[u]];
+        sum -= p[u]*sum_sons[u];
+        sum_sons[par[u]] -= 1-p[u]; 
         p[u] = new_p;
-        // for (int i = 0; i <= n; i++) cout << cont[i] << " \n"[i==n];
+        sum_sons[par[u]] += 1-p[u]; 
+        sum += p[u]*sum_sons[u];
+        sum += p[par[u]]*sum_sons[par[u]];
 
-        cout << sum << '\n';
+        printf("%.4lf\n", sum);
     }
 }
 
 signed main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-    cout << fixed << setprecision(4);
     solve();
     return 0;
 }
