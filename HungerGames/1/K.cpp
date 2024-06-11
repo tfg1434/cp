b1cbb053d269455cc91cede56da075fdf3b5c47b
torontofangirl <toronto.tristan@gmail.com>
2024-06-11 18:49:11 -0500

    pepsi cola counting subsets of masks with such OR

---
+++ HungerGames/1/K.cpp
@@ -0,0 +1,179 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#endif
+
+#define f first
+#define s second
+template<class T> using V = vector<T>; 
+using vi = V<int>;
+
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x) 
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+template<class T> int lwb(V<T>& a, const T& b) { return lb(all(a),b)-begin(a); }
+template<class T> int upb(V<T>& a, const T& b) { return ub(all(a),b)-begin(a); }
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a=b, true : false; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a=b, true : false; }
+
+using i64 = long long;
+template<class T>
+constexpr T pw(T a, i64 b) {
+    T res {1};
+    for (; b; b /= 2, a *= a) {
+        if (b % 2) {
+            res *= a;
+        }
+    }
+    return res;
+}
+constexpr i64 mul(i64 a, i64 b, i64 p) {
+    i64 res = a * b - i64(1.L * a * b / p) * p;
+    res %= p;
+    if (res < 0) {
+        res += p;
+    }
+    return res;
+}
+ 
+template<i64 P>
+struct MInt {
+    i64 x;
+    constexpr MInt() : x {0} {}
+    constexpr MInt(i64 x) : x {norm(x % getMod())} {}
+    
+    const static i64 Mod = 998244353;
+    constexpr static i64 getMod() {
+        if (P > 0) {
+            return P;
+        } else {
+            return Mod;
+        }
+    }
+    constexpr i64 norm(i64 x) const {
+        if (x < 0) {
+            x += getMod();
+        }
+        if (x >= getMod()) {
+            x -= getMod();
+        }
+        return x;
+    }
+    constexpr i64 val() const {
+        return x;
+    }
+    constexpr MInt operator-() const {
+        MInt res;
+        res.x = norm(getMod() - x);
+        return res;
+    }
+    constexpr MInt inv() const {
+        return pw(*this, getMod() - 2);
+    }
+    constexpr MInt &operator*=(MInt rhs) & {
+        if (getMod() < (1ULL << 31)) {
+            x = x * rhs.x % int(getMod());
+        } else {
+            x = mul(x, rhs.x, getMod());
+        }
+        return *this;
+    }
+    constexpr MInt &operator+=(MInt rhs) & {
+        x = norm(x + rhs.x);
+        return *this;
+    }
+    constexpr MInt &operator-=(MInt rhs) & {
+        x = norm(x - rhs.x);
+        return *this;
+    }
+    constexpr MInt &operator/=(MInt rhs) & {
+        return *this *= rhs.inv();
+    }
+    friend constexpr MInt operator*(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res *= rhs;
+        return res;
+    }
+    friend constexpr MInt operator+(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res += rhs;
+        return res;
+    }
+    friend constexpr MInt operator-(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res -= rhs;
+        return res;
+    }
+    friend constexpr MInt operator/(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res /= rhs;
+        return res;
+    }
+    friend constexpr istream &operator>>(istream &is, MInt &a) {
+        i64 v {};
+        is >> v;
+        a = MInt(v);
+        return is;
+    }
+    friend constexpr ostream &operator<<(ostream &os, const MInt &a) {
+        return os << a.val();
+    }
+    friend constexpr bool operator==(MInt lhs, MInt rhs) {
+        return lhs.val() == rhs.val();
+    }
+    friend constexpr bool operator!=(MInt lhs, MInt rhs) {
+        return lhs.val() != rhs.val();
+    }
+    friend constexpr bool operator<(MInt lhs, MInt rhs) {
+        return lhs.val() < rhs.val();
+    }
+};
+ 
+constexpr i64 mod = 1e9+7;
+using Z = MInt<mod>;
+
+// #pragma GCC target("bmi,bmi2,lzcnt,popcnt")
+#define pct __builtin_popcount
+constexpr int p2(int x) { return (int)1 << x; }
+constexpr int msk2(int x) { return p2(x)-1; }
+constexpr int bits(int x) { return x == 0 ? 0 : 31-__builtin_clz(x); } // floor(log2(x)) 
+
+int hmsm[p2(17)];
+Z hmor[p2(17)];
+
+void solve() {
+    int n; cin >> n; 
+    for (int i = 0; i < n; i++) {
+        int x; cin >> x;
+        hmsm[x]++;
+    }
+    for (int i = 0; i < 17; i++) {
+        for (int j = 0; j < p2(17); j++) {
+            if (j & p2(i)) hmsm[j] += hmsm[j^p2(i)];
+        }
+    }
+
+    for (int i = 0; i < p2(17); i++) {
+        hmor[i] = pw(Z(2), hmsm[i]) - 1;
+        for (int j = i; j > 0; j = ((j-1)&i)) if (j != i) {
+            hmor[i] -= hmor[j];
+        }
+    }
+
+    Z ans = 0;
+    for (int i = 0; i < p2(17); i++) {
+        ans += Z(i)*i*i * hmor[i];
+    }
+    cout << ans << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    solve();
+    return 0;
+}
