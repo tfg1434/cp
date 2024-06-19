767f09c11836efabb1957a01648eba5ac33e6efc
torontofangirl <toronto.tristan@gmail.com>
2024-06-19 16:29:34 -0500

    coci probability not to be in CH

---
+++ DMOJ/coci21c3p5.cpp
@@ -0,0 +1,191 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using i64 = long long;
+template<class T>
+constexpr T pw(T a, i64 b) {
+    T res {1};
+    for (; b; b /= 2, a *= a) {
+        if (b % 2) {
+            res *= a;
+        }
+    }
+    return res;
+}
+constexpr i64 mul(i64 a, i64 b, i64 p) {
+    i64 res = a * b - i64(1.L * a * b / p) * p;
+    res %= p;
+    if (res < 0) {
+        res += p;
+    }
+    return res;
+}
+ 
+template<i64 P>
+struct MInt {
+    i64 x;
+    constexpr MInt() : x {0} {}
+    constexpr MInt(i64 x) : x {norm(x % getMod())} {}
+    
+    const static i64 Mod = 998244353;
+    constexpr static i64 getMod() {
+        if (P > 0) {
+            return P;
+        } else {
+            return Mod;
+        }
+    }
+    constexpr i64 norm(i64 x) const {
+        if (x < 0) {
+            x += getMod();
+        }
+        if (x >= getMod()) {
+            x -= getMod();
+        }
+        return x;
+    }
+    constexpr i64 val() const {
+        return x;
+    }
+    constexpr MInt operator-() const {
+        MInt res;
+        res.x = norm(getMod() - x);
+        return res;
+    }
+    constexpr MInt inv() const {
+        return pw(*this, getMod() - 2);
+    }
+    constexpr MInt &operator*=(MInt rhs) & {
+        if (getMod() < (1ULL << 31)) {
+            x = x * rhs.x % int(getMod());
+        } else {
+            x = mul(x, rhs.x, getMod());
+        }
+        return *this;
+    }
+    constexpr MInt &operator+=(MInt rhs) & {
+        x = norm(x + rhs.x);
+        return *this;
+    }
+    constexpr MInt &operator-=(MInt rhs) & {
+        x = norm(x - rhs.x);
+        return *this;
+    }
+    constexpr MInt &operator/=(MInt rhs) & {
+        return *this *= rhs.inv();
+    }
+    friend constexpr MInt operator*(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res *= rhs;
+        return res;
+    }
+    friend constexpr MInt operator+(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res += rhs;
+        return res;
+    }
+    friend constexpr MInt operator-(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res -= rhs;
+        return res;
+    }
+    friend constexpr MInt operator/(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res /= rhs;
+        return res;
+    }
+    friend constexpr istream &operator>>(istream &is, MInt &a) {
+        i64 v {};
+        is >> v;
+        a = MInt(v);
+        return is;
+    }
+    friend constexpr ostream &operator<<(ostream &os, const MInt &a) {
+        return os << a.val();
+    }
+    friend constexpr bool operator==(MInt lhs, MInt rhs) {
+        return lhs.val() == rhs.val();
+    }
+    friend constexpr bool operator!=(MInt lhs, MInt rhs) {
+        return lhs.val() != rhs.val();
+    }
+    friend constexpr bool operator<(MInt lhs, MInt rhs) {
+        return lhs.val() < rhs.val();
+    }
+};
+ 
+constexpr i64 mod = 1e9+7;
+using Z = MInt<mod>;
+
+
+#define int int64_t
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#endif
+
+#define f first
+#define s second
+template<class T> using V = vector<T>; 
+using vi = V<int>;
+
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x) 
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+template<class T> int lwb(V<T>& a, const T& b) { return lb(all(a),b)-begin(a); }
+template<class T> int upb(V<T>& a, const T& b) { return ub(all(a),b)-begin(a); }
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a=b, true : false; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a=b, true : false; }
+
+using pt = complex<int>;
+#define x real()
+#define y imag()
+template<class T> istream& operator>>(istream& in, complex<T>& o) {
+    T X, Y; in >> X >> Y; o.real(X); o.imag(Y); return in;
+}
+template<class T> bool operator<(const complex<T>& a, const complex<T>& b) {
+    if (a.x == b.x) return a.y < b.y;
+    return a.x < b.x;
+}
+pt center;
+bool cmp(const pt& a, const pt& b) {
+    bool cat_a = a < center;
+    bool cat_b = b < center;
+    if (cat_a != cat_b) return a < b;
+    int cross = (conj(a-center)*(b-center)).y;
+    assert(cross);
+    return cross > 0;
+}
+
+void solve() {
+    int n; cin >> n;
+    V<pt> a(n); for (int i = 0; i < n; i++) cin >> a[i];
+
+    Z ans = 0;
+    for (int i = 0; i < n; i++) {
+        center = a[i];
+        auto b = a; b.erase(b.begin()+i);
+        sort(all(b), cmp);
+        b.insert(end(b), all(b));
+        Z cnt = 0;
+        for (int j = 0, k = 1; j < n-1; j++) {
+            if (k <= j) k = j+1;
+            while ((conj(b[j]-center)*(b[k]-center)).y > 0) {
+                k++;
+            } 
+            cnt += pw(Z(2), k-(j+1));
+        }
+        ans += pw(Z(2), n) - cnt - 1;
+    }
+
+    cout << ans << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    solve();
+    return 0;
+}
