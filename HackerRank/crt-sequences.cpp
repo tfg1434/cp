2770d0e9eb76a389d8d88f81e2396b3162ab5d78
torontofangirl <toronto.tristan@gmail.com>
2024-09-18 14:00:59 -0500

    HAcker rank congruence sequences

---
+++ HackerRank/crt-sequences.cpp
@@ -0,0 +1,220 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#endif
+
+#define f first
+#define s second
+template<class T> using V = vector<T>; 
+using vi = V<int>;
+using vb = V<bool>;
+using vs = V<string>;
+
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x) 
+#define len(x) (int)((x).size())
+#define rsz resize
+#define ins insert
+#define ft front()
+#define bk back()
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+template<class T> int lwb(V<T>& a, const T& b) { return lb(all(a),b)-begin(a); }
+template<class T> int upb(V<T>& a, const T& b) { return ub(all(a),b)-begin(a); }
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a=b, true : false; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a=b, true : false; }
+#define pct __builtin_popcount
+#define ctz __builtin_ctz
+#define clz __builtin_clz
+constexpr int p2(int x) { return (int)1 << x; }
+constexpr int bits(int x) { return x == 0 ? 0 : 31-clz(x); } // floor(log2(x)) 
+
+using i64 = long long;
+template<class T>
+constexpr T power(T a, i64 b) {
+    T res {1};
+    for (; b; b /= 2, a *= a) {
+        if (b % 2) {
+            res *= a;
+        }
+    }
+    return res;
+}
+constexpr i64 mul(i64 a, i64 b, i64 p) {
+    i64 res = a * b - i64(1.L * a * b / p) * p;
+    res %= p;
+    if (res < 0) {
+        res += p;
+    }
+    return res;
+}
+
+template<i64 P>
+struct MInt {
+    i64 x;
+    constexpr MInt() : x {0} {}
+    constexpr MInt(i64 x) : x {norm(x % getMod())} {}
+    
+    static inline i64 Mod;
+    constexpr static i64 getMod() {
+        if (P > 0) {
+            return P;
+        } else {
+            return Mod;
+        }
+    }
+    constexpr static void setMod(i64 Mod_) {
+        Mod = Mod_;
+    }
+    constexpr i64 norm(i64 x) const {
+        if (x < 0) {
+            x += getMod();
+        }
+        if (x >= getMod()) {
+            x -= getMod();
+        }
+        return x;
+    }
+    constexpr i64 val() const {
+        return x;
+    }
+    constexpr MInt operator-() const {
+        MInt res;
+        res.x = norm(getMod() - x);
+        return res;
+    }
+    constexpr MInt inv() const {
+        return power(*this, getMod() - 2);
+    }
+    constexpr MInt &operator*=(MInt rhs) & {
+        if (getMod() < (1ULL << 31)) {
+            x = x * rhs.x % int(getMod());
+        } else {
+            x = mul(x, rhs.x, getMod());
+        }
+        return *this;
+    }
+    constexpr MInt &operator+=(MInt rhs) & {
+        x = norm(x + rhs.x);
+        return *this;
+    }
+    constexpr MInt &operator-=(MInt rhs) & {
+        x = norm(x - rhs.x);
+        return *this;
+    }
+    constexpr MInt &operator/=(MInt rhs) & {
+        return *this *= rhs.inv();
+    }
+    friend constexpr MInt operator*(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res *= rhs;
+        return res;
+    }
+    friend constexpr MInt operator+(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res += rhs;
+        return res;
+    }
+    friend constexpr MInt operator-(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res -= rhs;
+        return res;
+    }
+    friend constexpr MInt operator/(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res /= rhs;
+        return res;
+    }
+    friend constexpr std::istream &operator>>(std::istream &is, MInt &a) {
+        i64 v = 69;
+        is >> v;
+        a = MInt(v);
+        return is;
+    }
+    friend constexpr std::ostream &operator<<(std::ostream &os, const MInt &a) {
+        return os << a.val();
+    }
+    friend constexpr bool operator==(MInt lhs, MInt rhs) {
+        return lhs.val() == rhs.val();
+    }
+    friend constexpr bool operator!=(MInt lhs, MInt rhs) {
+        return lhs.val() != rhs.val();
+    }
+    friend constexpr bool operator<(MInt lhs, MInt rhs) {
+        return lhs.val() < rhs.val();
+    }
+};
+ 
+template<>
+i64 MInt<0>::Mod = 998244353;
+ 
+constexpr int P = 1000000007;
+using Z = MInt<P>;
+
+int nu(int p, int n) {
+    int res = 0;
+    while (n%p == 0) {
+        res++;
+        n /= p;
+    }
+    return res;
+}
+
+const int N = 1e5;
+bool is_prime[N+1];
+void sieve() {
+    for (int i = 2; i <= N; i++) is_prime[i] = true;
+    for (int i = 2; i <= N; i++) if (is_prime[i]) {
+        for (int j = 2*i; j <= N; j += i) {
+            is_prime[j] = false;
+        }
+    }
+}
+
+void solve() {
+    int n; cin >> n;
+    vi a(n+1);
+    for (int i = 1; i <= n; i++) cin >> a[i];
+
+    Z ways = 1;
+    for (int i = 1; i <= n; i++) if (a[i] != -1) {
+        for (int j = 2*i; j <= n; j += i) if (a[j] != -1) {
+            if (a[j]%i != a[i]) ways = 0;
+        }
+    }
+
+    for (int p = 2; p <= n; p++) if (is_prime[p]) {
+        int alpha = 1;
+        {
+            int P = p;
+            while (p < 320 && P*p <= n) {
+                P *= p;
+                alpha++;
+            }
+        }
+        int beta = 0;
+        for (int i = p; i <= n; i += p) if (a[i] != -1) {
+            ckmax(beta, nu(p, i));
+        }
+
+        // if (p*p > n) {
+            // if (p*p > n && a[p] == -1) assert(alpha-beta==1);
+            // if (alpha-beta == 1) assert(p*p>n && a[p] == -1);
+        // }
+        ways *= power(p, alpha-beta);
+    }
+
+    cout << ways << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    sieve();
+    solve();
+    return 0;
+}
