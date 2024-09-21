f19757b001113c6704ad0a8b41db34bf5aeb2235
torontofangirl <toronto.tristan@gmail.com>
2024-09-21 13:48:05 -0500

    P5170

---
+++ Luogu/P5170.cpp
@@ -0,0 +1,199 @@
+#include <bits/stdc++.h>
+using namespace std;
+// #define int int64_t
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#endif
+
+#define f first
+#define s second
+template<class T> using V = vector<T>; 
+using vi = V<int>;
+using vb = V<bool>;
+using vs = V<string>;
+
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x) 
+#define len(x) (int)((x).size())
+#define rsz resize
+#define ins insert
+#define ft front()
+#define bk back()
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+template<class T> int lwb(V<T>& a, const T& b) { return lb(all(a),b)-begin(a); }
+template<class T> int upb(V<T>& a, const T& b) { return ub(all(a),b)-begin(a); }
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a=b, true : false; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a=b, true : false; }
+#define pct __builtin_popcount
+#define ctz __builtin_ctz
+#define clz __builtin_clz
+constexpr int p2(int x) { return (int)1 << x; }
+constexpr int bits(int x) { return x == 0 ? 0 : 31-clz(x); } // floor(log2(x)) 
+
+using i64 = long long;
+template<class T>
+constexpr T power(T a, i64 b) {
+    T res {1};
+    for (; b; b /= 2, a *= a) {
+        if (b % 2) {
+            res *= a;
+        }
+    }
+    return res;
+}
+constexpr i64 mul(i64 a, i64 b, i64 p) {
+    i64 res = a * b - i64(1.L * a * b / p) * p;
+    res %= p;
+    if (res < 0) {
+        res += p;
+    }
+    return res;
+}
+
+template<i64 P>
+struct MInt {
+    i64 x;
+    constexpr MInt() : x {0} {}
+    constexpr MInt(i64 x) : x {norm(x % getMod())} {}
+    
+    static inline i64 Mod;
+    constexpr static i64 getMod() {
+        if (P > 0) {
+            return P;
+        } else {
+            return Mod;
+        }
+    }
+    constexpr static void setMod(i64 Mod_) {
+        Mod = Mod_;
+    }
+    constexpr i64 norm(i64 x) const {
+        if (x < 0) {
+            x += getMod();
+        }
+        if (x >= getMod()) {
+            x -= getMod();
+        }
+        return x;
+    }
+    constexpr i64 val() const {
+        return x;
+    }
+    constexpr MInt operator-() const {
+        MInt res;
+        res.x = norm(getMod() - x);
+        return res;
+    }
+    constexpr MInt inv() const {
+        return power(*this, getMod() - 2);
+    }
+    constexpr MInt &operator*=(MInt rhs) & {
+        if (getMod() < (1ULL << 31)) {
+            x = x * rhs.x % int(getMod());
+        } else {
+            x = mul(x, rhs.x, getMod());
+        }
+        return *this;
+    }
+    constexpr MInt &operator+=(MInt rhs) & {
+        x = norm(x + rhs.x);
+        return *this;
+    }
+    constexpr MInt &operator-=(MInt rhs) & {
+        x = norm(x - rhs.x);
+        return *this;
+    }
+    constexpr MInt &operator/=(MInt rhs) & {
+        return *this *= rhs.inv();
+    }
+    friend constexpr MInt operator*(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res *= rhs;
+        return res;
+    }
+    friend constexpr MInt operator+(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res += rhs;
+        return res;
+    }
+    friend constexpr MInt operator-(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res -= rhs;
+        return res;
+    }
+    friend constexpr MInt operator/(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res /= rhs;
+        return res;
+    }
+    friend constexpr std::istream &operator>>(std::istream &is, MInt &a) {
+        i64 v = 69;
+        is >> v;
+        a = MInt(v);
+        return is;
+    }
+    friend constexpr std::ostream &operator<<(std::ostream &os, const MInt &a) {
+        return os << a.val();
+    }
+    friend constexpr bool operator==(MInt lhs, MInt rhs) {
+        return lhs.val() == rhs.val();
+    }
+    friend constexpr bool operator!=(MInt lhs, MInt rhs) {
+        return lhs.val() != rhs.val();
+    }
+    friend constexpr bool operator<(MInt lhs, MInt rhs) {
+        return lhs.val() < rhs.val();
+    }
+};
+ 
+template<>
+i64 MInt<0>::Mod = 998244353;
+using Z = MInt<0>;
+
+struct Num {
+    Z f, g, h;
+};
+
+#define int int64_t
+Z i2=499122177,i6=166374059;
+Num calc(int n, int a, int b, int c) {
+    Z ac = a/c, bc = b/c, m = (a*n+b)/c, n1 = n+1, n21 = n*2+1;
+    Num d;
+    if (a == 0) {
+        d.f = bc*n1;
+        d.g = n*n1*i2*bc;
+        d.h = bc*bc*n1;
+        return d;
+    }
+    if (a >= c || b >= c) {
+        d.f = n*n1*i2*ac + n1*bc;
+        d.g = n*n1*n21*i6*ac + n*n1*i2*bc;
+        d.h = n*n1*n21*i6*ac*ac + n1*bc*bc + n*n1*ac*bc;
+        auto e = calc(n, a%c, b%c, c);
+        d.f += e.f;
+        d.g += e.g;
+        d.h += e.h + 2*bc*e.f + 2*ac*e.g;
+        return d;
+    }
+    auto e = calc((a*n+b)/c-1,c,c-b-1,a);
+    d.f = n*m-e.f;
+    d.g = (m*n*n1 - e.h - e.f)*i2;
+    d.h = n*m*(m+1) - 2*e.g - 2*e.f - d.f;
+    return d;
+}
+
+void solve() {
+    int n, a, b, c; cin >> n >> a >> b >> c;
+    auto [f, g, h] = calc(n, a, b, c);
+    cout << f << ' ' << h << ' ' << g << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int tc; cin >> tc; while (tc--) solve();
+    return 0;
+}
